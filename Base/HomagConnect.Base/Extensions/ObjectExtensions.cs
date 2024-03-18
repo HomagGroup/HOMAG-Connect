f34df69d2defdaf75af065881885673a704233df
Wehrle, Boris <Boris.Wehrle@homag.com>
2024-03-18 13:24:59 +0100

    Extension methods added.

---
+++ Base/HomagConnect.Base/Extensions/ObjectExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+
+namespace HomagConnect.Base.Extensions
+{
+    /// <summary>
+    /// <see cref="object" /> extension methods.
+    /// </summary>
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Converts an object to a specified type by using JSON serialization.
+        /// </summary>
+        public static T ConvertTo<T>(this object o)
+        {
+            var serializedObject = JsonConvert.SerializeObject(o);
+            return JsonConvert.DeserializeObject<T>(serializedObject);
+        }
+
+        /// <summary>
+        /// Gets the value of a property.
+        /// </summary>
+        public static object GetPropertyValue(this object o, string propertyName)
+        {
+            object objValue = string.Empty;
+
+            var propertyInfo = o.GetType().GetProperty(propertyName);
+
+            if (propertyInfo != null)
+            {
+                objValue = propertyInfo.GetValue(o, null);
+            }
+
+            return objValue;
+        }
+    }
+}
