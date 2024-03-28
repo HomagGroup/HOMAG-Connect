eb35ccce577acbe9f9b7006ef3915078aa32668a
Robin Katzenberger <robin.katzenberger@homag.com>
2024-03-28 11:07:28 +0100

    refactor: remove unused usings

--- Applications/MaterialManager/Contracts/Material/Boards/Enumerations/CoatingCategory.cs
+++ Applications/MaterialManager/Contracts/Material/Boards/Enumerations/CoatingCategory.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace HomagConnect.MaterialManager.Contracts.Material.Boards.Enumerations
 {
@@ -9,24 +6,34 @@ namespace HomagConnect.MaterialManager.Contracts.Material.Boards.Enumerations
     {
         [Display(Description = "Aluminum")]
         Aluminium,
+
         [Display(Description = "Glass laminate (PMMA)")]
         LaminatedGlass,
+
         [Display(Description = "HPL")]
         HPL,
+
         [Display(Description = "Laminates consisting of decorative paper")]
         DecorativePaper,
+
         [Display(Description = "Melamine (thermoset)")]
         MelamineThermoSet,
+
         [Display(Description = "Painted")]
         Painted,
+
         [Display(Description = "Protective film")]
         ProtectiveFilm,
+
         [Display(Description = "Real metal")]
         RealMetal,
+
         [Display(Description = "Uncoated")]
         UnCoated,
+
         [Display(Description = "Undefined")]
         UnDefined,
+
         [Display(Description = "Veneer")]
         Veneer
     }
