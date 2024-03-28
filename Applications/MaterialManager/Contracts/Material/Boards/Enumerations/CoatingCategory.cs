40f9c5573fb52c781daa224a0e7291f4c226b0ae
Robin Katzenberger <robin.katzenberger@homag.com>
2024-03-28 11:02:55 +0100

    feat: add enum for coating category and material category

---
+++ Applications/MaterialManager/Contracts/Material/Boards/Enumerations/CoatingCategory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace HomagConnect.MaterialManager.Contracts.Material.Boards.Enumerations
+{
+    public enum CoatingCategory
+    {
+        [Display(Description = "Aluminum")]
+        Aluminium,
+        [Display(Description = "Glass laminate (PMMA)")]
+        LaminatedGlass,
+        [Display(Description = "HPL")]
+        HPL,
+        [Display(Description = "Laminates consisting of decorative paper")]
+        DecorativePaper,
+        [Display(Description = "Melamine (thermoset)")]
+        MelamineThermoSet,
+        [Display(Description = "Painted")]
+        Painted,
+        [Display(Description = "Protective film")]
+        ProtectiveFilm,
+        [Display(Description = "Real metal")]
+        RealMetal,
+        [Display(Description = "Uncoated")]
+        UnCoated,
+        [Display(Description = "Undefined")]
+        UnDefined,
+        [Display(Description = "Veneer")]
+        Veneer
+    }
+}
