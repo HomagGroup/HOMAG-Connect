2d4146a456b804b83b0a1b8856080cd6fa8134d2
Robin Katzenberger <robin.katzenberger@homag.com>
2024-04-05 17:21:22 +0200

    feature: add possible samples and tests

---
+++ Applications/MaterialManager/Tests/Read/Boards/MaterialManagerReadBoardsResult.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HomagConnect.MaterialManager.Samples.Read.Boards;
+using HomagConnect.MaterialManager.Tests.Base;
+
+namespace HomagConnect.MaterialManager.Tests.Read.Boards
+{
+    [TestClass]
+    [TestCategory("MaterialManager")]
+    [TestCategory("MaterialManager.Board.Read.Results")]
+    public class MaterialManagerReadBoardsResult : MaterialManagerTestBase
+    {
+        [TestMethod]
+        /// </summary>
+        public async Task GetMaterialCodes_GetResult_NoException()
+        {
+            var materialManager = GetMaterialManagerClient();
+
+            await MaterialManagerReadBoardsResults.GetMaterialCodes(materialManager);
+        }
+
+        [TestMethod]
+        public async Task GetThumbnails_GetResult_NoException()
+        {
+            var materialManager = GetMaterialManagerClient();
+
+            await MaterialManagerReadBoardsResults.GetThumbnails(materialManager);
+        }
+
+        [TestMethod]
+        public async Task GetLocations_GetResult_NoException()
+        {
+            var materialManager = GetMaterialManagerClient();
+
+            await MaterialManagerReadBoardsResults.GetLocations(materialManager, new List<string>{ "P2_Graphitschwarz_19.0_2800_2070", "P2_Lichtgrau_19_2800_2070" });
+        }
+    }
+}
