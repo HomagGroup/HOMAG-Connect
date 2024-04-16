92060b81a07ed9490eec1b6f553f381be00dd900
MirunaDanescu <162551966+MirunaDanescu@users.noreply.github.com>
2024-04-16 13:32:43 +0300

    intelliDivide nesting request using project (#39)
    * add patterns to solution
    
    * add nesting requests using project zip
    
    * correct test method

--- Applications/IntelliDivide/Tests/Requests/Nesting/NestingOptimizationRequestTests.cs
+++ Applications/IntelliDivide/Tests/Requests/Nesting/NestingOptimizationRequestTests.cs
@@ -1,4 +1,5 @@
 ﻿using HomagConnect.IntelliDivide.Contracts.Request;
+using HomagConnect.Base.Tests.Attributes;
 using HomagConnect.IntelliDivide.Samples.Requests.Nesting;
 using HomagConnect.IntelliDivide.Tests.Base;
 
@@ -9,8 +10,6 @@ namespace HomagConnect.IntelliDivide.Tests.Requests.Nesting
     [TestCategory("IntelliDivide.Requests.Nesting")]
     public class NestingOptimizationRequestTests : IntelliDivideTestBase
     {
-#pragma warning disable S2699 // Tests should include assertions
-
         [TestMethod]
         public async Task CreateNestingOptimizationRequestByObjectModel()
         {
@@ -34,7 +33,32 @@ namespace HomagConnect.IntelliDivide.Tests.Requests.Nesting
 
             await NestingOptimizationUsingObjectModel.CreateNestingOptimizationByObjectModelAndOptimizeAndSend(intelliDivide);
         }
-    }
 
-#pragma warning restore S2699 // Tests should include assertions
+        [TestMethod]
+        [TemporaryDisabledOnServer(2024, 5, 1)]
+        public async Task CreateNestingOptimizationUsingProjectZip()
+        {
+            var intelliDivide = GetIntelliDivideClient();
+
+            await NestingOptimizationUsingProjectZip.CreateNestingOptimizationUsingProjectZip(intelliDivide);
+        }
+
+        [TestMethod]
+        [TemporaryDisabledOnServer(2024, 5, 1)]
+        public async Task CreateNestingOptimizationUsingProjectZipAndOptimize()
+        {
+            var intelliDivide = GetIntelliDivideClient();
+
+            await NestingOptimizationUsingProjectZip.CreateNestingOptimizationUsingProjectZipAndOptimize(intelliDivide);
+        }
+
+        [TestMethod]
+        [TemporaryDisabledOnServer(2024, 5, 1)]
+        public async Task CreateNestingOptimizationUsingProjectZipAndOptimizeAndSend()
+        {
+            var intelliDivide = GetIntelliDivideClient();
+
+            await NestingOptimizationUsingProjectZip.CreateNestingOptimizationUsingProjectZipAndOptimizeAndSend(intelliDivide);
+        }
+    }
 }
