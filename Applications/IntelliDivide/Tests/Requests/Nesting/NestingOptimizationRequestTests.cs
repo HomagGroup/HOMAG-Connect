9ac24414fe0169a1cb653b88e395645cff3f9abe
Miruna Danescu <miruna.danescu@accesa.eu>
2024-04-16 09:40:23 +0300

    add nesting requests using project zip

---
+++ Applications/IntelliDivide/Tests/Requests/Nesting/NestingOptimizationRequestTests.cs
@@ -0,0 +1,39 @@
+﻿using HomagConnect.Base.Tests.Attributes;
+using HomagConnect.IntelliDivide.Samples.Requests.Nesting;
+using HomagConnect.IntelliDivide.Tests.Base;
+
+namespace HomagConnect.IntelliDivide.Tests.Requests.Nesting
+{
+    [TestClass]
+    [TestCategory("IntelliDivide")]
+    [TestCategory("IntelliDivide.Requests.Nesting")]
+    public class NestingOptimizationRequestTests : IntelliDivideTestBase
+    {
+        [TestMethod]
+        [TemporaryDisabledOnServer(2024, 5, 1)]
+        public async Task CreateNestingOptimizationUsingProjectZip()
+        {
+            var intelliDivide = GetIntelliDivideClient();
+
+            await NestingOptimizationUsingProjectZip.CreateNestingOptimizationUsingProjectZip(intelliDivide);
+        }
+
+        [TestMethod]
+        [TemporaryDisabledOnServer(2024, 5, 1)]
+        public async Task CreateNestingOptimizationUsingProjectZipAndOptimize()
+        {
+            var intelliDivide = GetIntelliDivideClient();
+
+            await NestingOptimizationUsingProjectZip.CreateNestingOptimizationUsingProjectZipAndOptimize(intelliDivide);
+        }
+
+        [TestMethod]
+        [TemporaryDisabledOnServer(2024, 5, 1)]
+        public async Task CreateNestingOptimizationUsingProjectZipAndOptimizeAndSend()
+        {
+            var intelliDivide = GetIntelliDivideClient();
+
+            await NestingOptimizationUsingProjectZip.CreateNestingOptimizationUsingProjectZipAndOptimizeAndSend(intelliDivide);
+        }
+    }
+}
