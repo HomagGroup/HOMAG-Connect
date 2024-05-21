d4ba4e21bbc1c62ff4adf99d669b73c2fd30190b
Robin Katzenberger <robin.katzenberger@homag.com>
2024-05-21 16:11:26 +0200

    feat: create client interface for MaterialManagerClientProcessingOptimization

---
+++ Applications/MaterialManager/Contracts/Processing/Interfaces/IMaterialManagerClientProcessingOptimization.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using HomagConnect.MaterialManager.Contracts.Processing.Optimization;
+
+namespace HomagConnect.MaterialManager.Contracts.Processing.Interfaces
+{
+    /// <summary>
+    /// Interface for materialManager Client for processing optimization
+    /// </summary>
+    public interface IMaterialManagerClientProcessingOptimization
+    {
+        /// <summary>
+        /// Get offcut parameter set for the given material code
+        /// </summary>
+        /// <param name="materialCode"></param>
+        /// <returns></returns>
+        Task<OffcutParameterSet> GetOffcutParameterSetAsync(string materialCode);
+
+        /// <summary>
+        /// Get offcut parameter sets for the given material codes
+        /// </summary>
+        /// <param name="materialCodes"></param>
+        /// <returns></returns>
+        Task<IEnumerable<OffcutParameterSet>> GetOffcutParameterSetsAsync(ICollection<string> materialCodes);
+    }
+}
