5d3f85b3c6958a7a7387f557ec01d65ba94a46f6
Boris Wehrle (HOMAG) <Boris.Wehrle@homag.com>
2024-06-05 13:59:24 +0200

    Tests disabled

--- Applications/IntelliDivide/Tests/Requests/Nesting/NestingOptimizationRequestTests.cs
+++ Applications/IntelliDivide/Tests/Requests/Nesting/NestingOptimizationRequestTests.cs
@@ -41,7 +41,7 @@ namespace HomagConnect.IntelliDivide.Tests.Requests.Nesting
         }
 
         [TestMethod]
-        [TemporaryDisabledOnServer(2024, 6, 1)] // todo: reenable tests divide!
+        [TemporaryDisabledOnServer(2024, 6, 15)] // todo: reenable tests divide!
         public async Task NestingRequest_Template_CSV_MPR_ImportAndOptimize()
         {
             var intelliDivide = GetIntelliDivideClient();
