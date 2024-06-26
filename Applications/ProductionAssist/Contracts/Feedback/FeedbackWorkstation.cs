901972434319dd8bb32d12e8d7b25ff62b84fb51
ACCESA\fabian.ardeiu <fabian.ardeiu@accesa.eu>
2024-06-26 11:36:00 +0300

    Enhance pAFeedbackClient

--- Applications/ProductionAssist/Contracts/Feedback/FeedbackWorkstation.cs
+++ Applications/ProductionAssist/Contracts/Feedback/FeedbackWorkstation.cs
@@ -1,8 +1,23 @@
 ﻿namespace HomagConnect.ProductionAssist.Contracts.Feedback
 {
+    /// <summary>
+    /// Feedback Workstation
+    /// </summary>
     public class FeedbackWorkstation
     {
+        /// <summary>
+        /// Gets or sets the Workstation Id
+        /// </summary>
         public Guid Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the display name
+        /// </summary>
+        public string? DisplayName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tapio machine ID
+        /// </summary>
+        public string? AssignedTapioMachineId { get; set; }
     }
 }
