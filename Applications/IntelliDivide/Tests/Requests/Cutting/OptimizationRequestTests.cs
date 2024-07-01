7116b311a0676e5fb07c9102c168f4bd17cda411
Boris Wehrle <Boris.Wehrle@homag.com>
2024-07-01 11:08:19 +0200

    Missed tests disabled

--- Applications/IntelliDivide/Tests/Requests/Cutting/OptimizationRequestTests.cs
+++ Applications/IntelliDivide/Tests/Requests/Cutting/OptimizationRequestTests.cs
@@ -38,7 +38,7 @@ public class CuttingOptimizationRequestTests : IntelliDivideTestBase
     }
 
     [TestMethod]
-    [TemporaryDisabledOnServer(2024,7,1)]
+    [TemporaryDisabledOnServer(2024,7,15)]
     public async Task CuttingRequest_ObjectModel_GrainMatchingTemplate_ImportOnly()
     {
         var intelliDivide = GetIntelliDivideClient();
@@ -106,7 +106,7 @@ public class CuttingOptimizationRequestTests : IntelliDivideTestBase
     }
 
     [TestMethod]
-    [TemporaryDisabledOnServer(2024,7,1)]
+    [TemporaryDisabledOnServer(2024, 7, 15)]
     public async Task CreateCuttingOptimizationByObjectModelOptimizeAndRetrieveResults()
     {
         var intelliDivide = GetIntelliDivideClient();
