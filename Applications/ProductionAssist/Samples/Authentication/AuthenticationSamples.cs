1684eb0efa7d8f93945fe77f252a3728a55a12ee
ACCESA\fabian.ardeiu <fabian.ardeiu@accesa.eu>
2024-07-08 09:36:42 +0300

    Added authentication samples/test/doc file

---
+++ Applications/ProductionAssist/Samples/Authentication/AuthenticationSamples.cs
@@ -0,0 +1,52 @@
+﻿using HomagConnect.Base.Extensions;
+using HomagConnect.ProductionAssist.Client;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace HomagConnect.ProductionAssist.Samples.Authentication
+{
+    /// <summary>
+    /// Authentication samples.
+    /// <remarks>
+    /// <see href="https://github.com/HomagGroup/HOMAG-Connect/tree/main/Applications/ProductionManager/Samples/Authentication" /> for further details.
+    /// </remarks>
+    /// </summary>
+    public static class AuthenticationSamples
+    {
+        /// <summary>
+        /// Create ProductionAssist client using HttpClient.
+        /// </summary>
+        public static async Task CreateProductionAssistClientUsingHttpClient(Guid subscriptionId, string authorizationKey)
+        {
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://connect.homag.cloud")
+            };
+
+            var authenticationHeaderValue = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{subscriptionId}:{authorizationKey}"));
+
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authenticationHeaderValue);
+
+            var productionAssist = new ProductionAssistFeedbackClient(httpClient);
+
+            var workstations = await productionAssist.GetWorkstationsAsync();
+
+            workstations.Trace();
+        }
+
+        /// <summary>
+        /// Create ProductionAssist client using subscription id and authorization key.
+        /// </summary>
+        public static async Task CreateProductionAssistClientUsingSubscriptionIdAndToken(Guid subscriptionId, string authorizationKey)
+        {
+            var productionManager = new ProductionAssistFeedbackClient(subscriptionId, authorizationKey);
+
+            var workstations = await productionManager.GetWorkstationsAsync();
+
+            workstations.Trace();
+        }
+    }
+
+}
---
+++ Applications/ProductionAssist/Tests/Authentication/AuthenticationTests.cs
@@ -0,0 +1,28 @@
+﻿using HomagConnect.Base.Tests.Attributes;
+using HomagConnect.ProductionAssist.Samples.Authentication;
+
+namespace HomagConnect.ProductionAssist.Tests.Authentication
+{
+    [TestClass]
+    [TestCategory("ProductionAssist")]
+    [TestCategory("ProductionAssist.Authentication")]
+    [TemporaryDisabledOnServer(2024, 7, 22)]
+    public class AuthenticationTests : ProductionAssistTestBase
+    {
+#pragma warning disable S2699 // Tests should include assertions
+
+        [TestMethod]
+        public async Task Authentication_UsingHttpClient()
+        {
+            await AuthenticationSamples.CreateProductionAssistClientUsingHttpClient(SubscriptionId, AuthorizationKey);
+        }
+
+        [TestMethod]
+        public async Task Authentication_UsingSubscriptionIdAndToken()
+        {
+            await AuthenticationSamples.CreateProductionAssistClientUsingSubscriptionIdAndToken(SubscriptionId, AuthorizationKey);
+        }
+
+#pragma warning restore S2699 // Tests should include assertions
+    }
+}
