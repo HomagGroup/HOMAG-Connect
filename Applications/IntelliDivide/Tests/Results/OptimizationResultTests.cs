ce5e86f8e9c239c34ee49fff7aed8459c1e61d9e
Büche, Johannes <Johannes.Bueche@homag.com>
2024-07-17 06:59:28 +0200

    tests disabled

--- Applications/IntelliDivide/Tests/Results/OptimizationResultTests.cs
+++ Applications/IntelliDivide/Tests/Results/OptimizationResultTests.cs
@@ -42,7 +42,7 @@ public class OptimizationResultTests : IntelliDivideTestBase
 
     /// <summary />
     [TestMethod]
-    [TemporaryDisabledOnServer(2024, 7, 15)] // todo: reenable tests divide!
+    [TemporaryDisabledOnServer(2024, 9, 1)] // todo: reenable tests divide!
     public async Task Solution_GetDetails_NoException()
     {
         var intelliDivide = GetIntelliDivideClient();
@@ -52,7 +52,7 @@ public class OptimizationResultTests : IntelliDivideTestBase
 
     /// <summary />
     [TestMethod]
-    [TemporaryDisabledOnServer(2024, 7, 15)] // todo: reenable tests divide!
+    [TemporaryDisabledOnServer(2024, 9, 1)] // todo: reenable tests divide!
     public async Task Solution_List_NoException()
     {
         var intelliDivide = GetIntelliDivideClient();
