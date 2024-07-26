6331c476ee3138511f3e07606b4a929bd7248204
Boris Wehrle <Boris.Wehrle@homag.com>
2024-07-26 11:16:47 +0200

    TolerantEnumConverter added to AdditionalDataType

--- Base/HomagConnect.Base.Contracts/AdditionalData/AdditionalDataType.cs
+++ Base/HomagConnect.Base.Contracts/AdditionalData/AdditionalDataType.cs
@@ -1,10 +1,15 @@
-﻿namespace HomagConnect.Base.Contracts.AdditionalData;
+﻿using HomagConnect.Base.Contracts.Converter;
+
+using Newtonsoft.Json;
+
+namespace HomagConnect.Base.Contracts.AdditionalData;
 
 // Note: This is preliminary code and is subject to change
 
 /// <summary>
 /// Gets or sets the additional data type.
 /// </summary>
+[JsonConverter(typeof(TolerantEnumConverter))]
 public enum AdditionalDataType
 {
     /// <summary />
