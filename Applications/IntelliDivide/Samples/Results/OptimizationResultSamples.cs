1f6812a4d5bac0bef6a986e6a12b843cc6acc011
Boris Wehrle <Boris.Wehrle@homag.com>
2024-08-20 08:49:15 +0200

    Tests stabilized

--- Applications/IntelliDivide/Samples/Results/OptimizationResultSamples.cs
+++ Applications/IntelliDivide/Samples/Results/OptimizationResultSamples.cs
@@ -73,6 +73,11 @@ namespace HomagConnect.IntelliDivide.Samples.Results
         {
             var optimization = await intelliDivide.GetOptimizationsAsync(OptimizationType.Cutting, OptimizationStatus.Optimized, _Take).FirstAsync();
 
+            if (optimization == null)
+            {
+                Assert.Inconclusive("No optimized having the state optimization found.");
+            }
+
             var optimizationSolutions = await intelliDivide.GetSolutionsAsync(optimization.Id).ToListAsync();
 
             var balancedSolution = optimizationSolutions.Where(s => s.Name == SolutionName.BalancedSolution);
@@ -86,6 +91,11 @@ namespace HomagConnect.IntelliDivide.Samples.Results
         public static async Task GetSolutionsSample(IIntelliDivideClient intelliDivide)
         {
             var optimization = await intelliDivide.GetOptimizationsAsync(OptimizationType.Cutting, OptimizationStatus.Optimized, _Take).FirstAsync(o => o.Status == OptimizationStatus.Optimized);
+
+            if (optimization == null)
+            {
+                Assert.Inconclusive("No optimized having the state optimization found.");
+            }
 
             var optimizationSolutions = await intelliDivide.GetSolutionsAsync(optimization.Id).ToListAsync();
 
