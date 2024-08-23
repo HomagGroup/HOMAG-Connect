7814974b9e48cc8395917d05d1bc7d0a9649e939
Boris Wehrle <Boris.Wehrle@homag.com>
2024-08-23 08:30:59 +0200

    Tests disabled

--- Applications/MaterialAssist/Tests/Authentication/AuthenticationTests.cs
+++ Applications/MaterialAssist/Tests/Authentication/AuthenticationTests.cs
@@ -1,14 +1,16 @@
-﻿using HomagConnect.MaterialAssist.Samples.Authentication;
+﻿using HomagConnect.Base.Tests.Attributes;
+using HomagConnect.MaterialAssist.Samples.Authentication;
 
 namespace HomagConnect.MaterialAssist.Tests.Authentication
 {
     [TestClass]
     [TestCategory("MaterialAssist")]
     [TestCategory("MaterialAssist.Authentication")]
+    [TemporaryDisabledOnServer(2024, 9, 15)]
     public class AuthenticationTests : MaterialAssistTestBase
     {
 #pragma warning disable S2699 // Tests should include assertions
-
+        
         [TestMethod]
         public async Task Authentication_UsingHttpClient()
 
