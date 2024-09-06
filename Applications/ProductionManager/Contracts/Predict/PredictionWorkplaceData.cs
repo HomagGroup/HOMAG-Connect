f996ff2b43802c04d1fbd850c6e1ba354417c83c
Oana Sabadas <oana.sabadas@accesa.eu>
2024-09-06 11:44:59 +0300

    add corecutting prediction based on workplace data for productionManager prediction for lot generation

---
+++ Applications/ProductionManager/Contracts/Predict/PredictionWorkplaceData.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace HomagConnect.ProductionManager.Contracts.Predict;
+
+/// <summary>
+/// Result for one machine
+/// </summary>
+public class PredictionWorkplaceData
+{
+    /// <summary>
+    /// requested workplaceId
+    /// </summary>    
+    [JsonProperty(Order = 1)]
+    public Guid WorkplaceId { get; set; }
+
+    /// <summary>
+    /// The minimum estimated duration calculated based on Quartile 1.
+    /// </summary>
+    [JsonProperty(Order = 2)]
+    public TimeSpan? DurationMin { get; set; }
+
+    /// <summary>
+    /// Predicted edgebanding duration.
+    /// </summary>
+    [JsonProperty(Order = 3)]
+    public TimeSpan? Duration { get; set; }
+
+    /// <summary>
+    /// The maximum estimated duration calculated based on Quartile 3.
+    /// </summary>
+    [JsonProperty(Order = 4)]
+    public TimeSpan? DurationMax { get; set; }
+
+}
