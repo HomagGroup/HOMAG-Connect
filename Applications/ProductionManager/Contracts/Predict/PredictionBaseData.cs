c2174c8e8ebd7641946a8b567b29283fc1a4d586
Oana Sabadas <35168370+oanasabadas@users.noreply.github.com>
2024-09-13 18:47:22 +0300

    add core cutting prediction model based on workplace data for pM (#152)
    * add corecutting prediction based on workplace data for productionManager prediction for lot generation
    
    * reconfigure models using base classes
    
    ---------
    
    Co-authored-by: Oana Sabadas <oana.sabadas@accesa.eu>

---
+++ Applications/ProductionManager/Contracts/Predict/PredictionBaseData.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System.Runtime.Serialization;
+
+namespace HomagConnect.ProductionManager.Contracts.Predict;
+
+/// <summary>
+/// Result for one machine
+/// </summary>
+public class PredictionBaseData
+{
+    /// <summary>
+    /// The minimum estimated duration calculated based on Quartile 1.
+    /// </summary>
+    [JsonProperty(Order = 3)]
+    public TimeSpan? DurationMin { get; set; }
+
+    /// <summary>
+    /// Predicted edgebanding duration.
+    /// </summary>
+    [JsonProperty(Order = 4)]
+    public TimeSpan? Duration { get; set; }
+
+    /// <summary>
+    /// The maximum estimated duration calculated based on Quartile 3.
+    /// </summary>
+    [JsonProperty(Order = 5)]
+    public TimeSpan? DurationMax { get; set; }
+
+
+    public virtual string DataSourceType { get; set; }
+}
