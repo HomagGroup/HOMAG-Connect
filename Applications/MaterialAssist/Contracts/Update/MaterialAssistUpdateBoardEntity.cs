5ea244cc1eb5a335e6605bfc3ab8d2c5f6f29010
Robin Katzenberger <robin.katzenberger@homag.com>
2024-09-16 14:31:35 +0200

    feat: add client functionality for updating board entities

---
+++ Applications/MaterialAssist/Contracts/Update/MaterialAssistUpdateBoardEntity.cs
@@ -0,0 +1,24 @@
+﻿namespace HomagConnect.MaterialAssist.Contracts.Update
+{
+    public class MaterialAssistUpdateBoardEntity
+    {
+        public string? Comments { get; set; }
+
+        public string? ExternalId { get; set; }
+
+        /// <summary>
+        /// Only available for single boards
+        /// </summary>
+        public double? Length { get; set; }
+
+        /// <summary>
+        /// Only available for stacks or goods in stock
+        /// </summary>
+        public int? Quantity { get; set; }
+
+        /// <summary>
+        /// Only available for single boards
+        /// </summary>
+        public double? Width { get; set; }
+    }
+}
