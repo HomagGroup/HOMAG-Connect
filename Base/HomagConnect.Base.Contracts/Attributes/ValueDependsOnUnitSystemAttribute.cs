0b0bf7dcb581aafce2250186d58e4f9e0f18c516
Daniel Schaber <daniel@schaber-medien.de>
2024-09-23 16:47:14 +0200

    cleanup resharper

--- Base/HomagConnect.Base.Contracts/Attributes/ValueDependsOnUnitSystemAttribute.cs
+++ Base/HomagConnect.Base.Contracts/Attributes/ValueDependsOnUnitSystemAttribute.cs
@@ -7,11 +7,15 @@ public sealed class ValueDependsOnUnitSystemAttribute : Attribute
 {
     /// <summary>
     /// Attribute to define the unit system of a property.
-    /// Please pass a <see cref="metricDecimalPrecision"/> or <see cref="imperialDecimalPrecision"/> value if you want to override the default decimals.
+    /// Please pass a <see cref="metricDecimalPrecision" /> or <see cref="imperialDecimalPrecision" /> value if you want to
+    /// override the default decimals.
     /// </summary>
     /// <param name="baseUnit">The unit system of that property.</param>
     /// <param name="metricDecimalPrecision">Defines how many decimals after comma are shown in metric system. Default is 1.</param>
-    /// <param name="imperialDecimalPrecision">Defines how many decimals after comma are shown in imperial system. Default is 3.</param>
+    /// <param name="imperialDecimalPrecision">
+    /// Defines how many decimals after comma are shown in imperial system. Default is
+    /// 3.
+    /// </param>
     public ValueDependsOnUnitSystemAttribute(BaseUnit baseUnit, int metricDecimalPrecision = DecimalPrecision.OneDecimalPlace, int imperialDecimalPrecision = DecimalPrecision.ThreeDecimalPlaces)
     {
         BaseUnit = baseUnit;
@@ -21,7 +25,7 @@ public sealed class ValueDependsOnUnitSystemAttribute : Attribute
 
     public BaseUnit BaseUnit { get; }
 
-    public int MetricDecimalPrecision { get; }
-
     public int ImperialDecimalPrecision { get; }
+
+    public int MetricDecimalPrecision { get; }
 }
