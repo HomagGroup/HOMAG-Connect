4421b31f316c3b9d758a9ad2d9e4daed357c4d8b
ACCESA\fabian.ardeiu <fabian.ardeiu@accesa.eu>
2024-11-11 12:32:40 +0200

    Added produciton entity properties

---
+++ Applications/ProductionManager/Contracts/ProductionEntity/ProductionEntityGrain.cs
@@ -0,0 +1,26 @@
+﻿using HomagConnect.Base.Contracts.Converter;
+
+using Newtonsoft.Json;
+
+namespace HomagConnect.ProductionManager.Contracts.ProductionEntity
+{
+    /// <summary>
+    /// Production entity grain
+    /// </summary>
+    [JsonConverter(typeof(TolerantEnumConverter))]
+    public enum ProductionEntityGrain
+    {
+        /// <summary>
+        /// No grain
+        /// </summary>
+        NoGrain = 0,
+        /// <summary>
+        /// Length wise
+        /// </summary>
+        Lengthwise = 1,
+        /// <summary>
+        /// Cross wise
+        /// </summary>
+        Crosswise = 2
+    }
+}
