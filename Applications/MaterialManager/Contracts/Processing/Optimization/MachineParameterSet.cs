b8b0b6cd65f5ff892cd98a0f840228290fe35fc8
Alexandru Marcu <alexandru.marcu@cadrantechnologies.eu>
2024-11-21 15:03:11 +0200

    feat: machine parameter models

---
+++ Applications/MaterialManager/Contracts/Processing/Optimization/MachineParameterSet.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+using HomagConnect.Base.Contracts.Enumerations;
+using HomagConnect.Base.Contracts.Interfaces;
+
+using Newtonsoft.Json;
+
+namespace HomagConnect.MaterialManager.Contracts.Processing.Optimization;
+
+/// <summary>
+/// Represents the parameters for the machine optimization.
+/// </summary>
+public class MachineParameterSet : IValidatableObject, IContainsUnitSystemDependentProperties
+{
+    private const int _MaterialGroupNameMaxLength = 50;
+
+    /// <summary>
+    /// Gets or sets the <see cref="IsUnassignedMaterialsGroup" /> which determines if the group contains all materials which
+    /// are not manually assigned to a group.
+    /// </summary>
+    [JsonProperty(Order = 4)]
+    public bool IsUnassignedMaterialsGroup { get; set; }
+
+    /// <summary>
+    /// Gets or sets the material codes for which the <see cref="MachineParameterSet" /> is valid.
+    /// </summary>
+    [JsonProperty(Order = 2)]
+    [MinLength(1)]
+    public string[] MaterialCodes { get; set; } = Array.Empty<string>();
+
+    /// <summary>
+    /// Gets or sets the name of the material group.
+    /// </summary>
+    [Key]
+    [JsonProperty(Order = 1)]
+    [Required]
+    [StringLength(_MaterialGroupNameMaxLength, MinimumLength = 1)]
+    public string MaterialGroupName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the <see cref="MaterialManagerLink" />.
+    /// </summary>
+    [JsonProperty(Order = 3)]
+    public string MaterialManagerLink { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the <see cref="MachineParameters" />.
+    /// </summary>
+    [JsonProperty(Order = 11)]
+    public MachineParameters Parameters { get; set; } = new();
+
+    #region IContainsUnitSystemDependentProperties Members
+
+    /// <inheritdoc />
+    [JsonProperty(Order = 30)]
+    public UnitSystem UnitSystem { get; set; } = UnitSystem.Metric;
+
+    #endregion
+
+    #region IValidatableObject Members
+
+    /// <inheritdoc />
+#pragma warning disable S3776 // Cognitive Complexity of methods should not be too high
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+#pragma warning restore S3776 // Cognitive Complexity of methods should not be too high
+    {
+        return new List<ValidationResult>();
+    }
+
+    #endregion
+}
---
+++ Applications/MaterialManager/Contracts/Processing/Optimization/MachineParameters.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace HomagConnect.MaterialManager.Contracts.Processing.Optimization;
+
+/// <summary>
+/// Model for material dependent machine parameters.
+/// </summary>
+public class MachineParameters
+{
+    /// <summary>
+    /// Gets or sets the machine parameter.
+    /// </summary>
+    [JsonProperty(Order = 10)]
+    public string? MachineParameter { get; set; }
+}
