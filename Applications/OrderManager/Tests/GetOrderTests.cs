2203e0ef4e1ae37e5110f351aaf8965c8c6bf330
Boris Wehrle <Boris.Wehrle@homag.com>
2025-01-29 14:54:43 +0100

    Sample test added.

---
+++ Applications/OrderManager/Tests/GetOrderTests.cs
@@ -0,0 +1,25 @@
+﻿using HomagConnect.Base.Extensions;
+using HomagConnect.OrderManager.Contracts;
+
+namespace HomagConnect.OrderManager.Tests
+{
+    [TestClass]
+    [TestCategory("OrderManager")]
+    [TestCategory("OrderManager.Orders")]
+    public sealed class GetOrderTests
+    {
+        [TestMethod]
+        public void GetOrder()
+        {
+            var order = new Order();
+
+            // Header
+
+            order.OrderName = "Bedroom & bathroom 01";
+            order.Project = "Single family house Müller John";
+            order.PersonInCharge = "Hendrik Albers";
+
+            order.Trace(nameof(order));
+        }
+    }
+}
