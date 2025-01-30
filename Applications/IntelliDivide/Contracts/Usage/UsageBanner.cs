9e729687f987afec318db9be7fac88d8f76639b9
Matei-Lazar <49794521+Matei-Lazar@users.noreply.github.com>
2025-01-30 17:26:13 +0200

    Create UsageBanner.cs

---
+++ Applications/IntelliDivide/Contracts/Usage/UsageBanner.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace HomagConnect.IntelliDivide.Contracts.Usage
+{
+    /// <summary>
+    /// Shows the number of transferred parts and the licenses owned in the current period
+    /// </summary>
+    public class UsageBanner
+    {
+        /// <summary>
+        /// The number of parts transferred in the current period
+        /// </summary>
+		[JsonProperty(Order = 3)]
+        public int NumberOfPartsTransferred { get; set; }
+
+        /// <summary>
+        /// The licenses owned in the current period
+        /// </summary>
+		[JsonProperty(Order = 4)]
+        public IEnumerable<LicenseDetails> Licenses { get; set; }
+    }
+}
