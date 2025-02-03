db2cc1c97c6b86af1877a31236082e0c1febf064
Matei-Lazar <49794521+Matei-Lazar@users.noreply.github.com>
2025-02-03 10:07:16 +0200

    Update UsageDetails.cs

--- Applications/IntelliDivide/Contracts/Usage/UsageDetails.cs
+++ Applications/IntelliDivide/Contracts/Usage/UsageDetails.cs
@@ -11,43 +11,43 @@ namespace HomagConnect.IntelliDivide.Contracts.Usage
         /// <summary>
         /// The number of parts transferred in the job
         /// </summary>
-		[JsonProperty(Order = 1)]
+	[JsonProperty(Order = 1)]
         public int NumberOfPartsTransferred { get; set; }
 
         /// <summary>
         /// The time when the job was transferred
         /// </summary>
-		[JsonProperty(Order = 2)]
+	[JsonProperty(Order = 2)]
         public DateTimeOffset JobTransferredAt { get; set; }
 
         /// <summary>
         /// The name of the job
         /// </summary>
-		[JsonProperty(Order = 3)]
+	[JsonProperty(Order = 3)]
         public string JobName { get; set; }
 
         /// <summary>
         /// The user who transferred the job
         /// </summary>
-		[JsonProperty(Order = 4)]
+	[JsonProperty(Order = 4)]
         public string JobTransferredBy { get; set; }
 
         /// <summary>
         /// The name of the machine that will process the job
         /// </summary>
-		[JsonProperty(Order = 5)]
+	[JsonProperty(Order = 5)]
         public string MachineName { get; set; }
 
         /// <summary>
         /// The board waste percentage
         /// </summary>
-		[JsonProperty(Order = 6)]
+	[JsonProperty(Order = 6)]
         public double? WastePercentage { get; set; }
 
         /// <summary>
         /// The time estimation for the job to be processed
         /// </summary>
-		[JsonProperty(Order = 7)]
+	[JsonProperty(Order = 7)]
         public double? PredictedProductionTimeInSec { get; set; }
     }
 }
