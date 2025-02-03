d700d36e69af6e6b72e7fad1e0d9adc6860e0dd2
Matei-Lazar <49794521+Matei-Lazar@users.noreply.github.com>
2025-02-03 13:30:48 +0200

    Update UsageOverview.cs

--- Applications/IntelliDivide/Contracts/Usage/UsageOverview.cs
+++ Applications/IntelliDivide/Contracts/Usage/UsageOverview.cs
@@ -6,18 +6,24 @@ namespace HomagConnect.IntelliDivide.Contracts.Usage
     /// <summary>
     /// Describes the overview per period of the usage of the owned licenses and packages
     /// </summary>
-    public class UsageOverview : UsageBanner
+    public class UsageOverview
     {
-        /// <summary>
-	/// The id of the subscription
-	/// </summary>
-	[JsonProperty(Order = 1)]
-	public Guid SubscriptionId { get; set; }
-
 	/// <summary>
 	/// The month of the usage overview
 	/// </summary>
-	[JsonProperty(Order = 2)]
+	[JsonProperty(Order = 1)]
 	public DateTime Period { get; set; }
+
+ 	/// <summary>
+        /// The number of parts transferred in the current period
+        /// </summary>
+	[JsonProperty(Order = 2)]
+        public int? NumberOfPartsTransferred { get; set; }
+
+        /// <summary>
+        /// The licenses owned in the current period
+        /// </summary>
+	[JsonProperty(Order = 3)]
+        public IEnumerable<LicenseDetails> Licenses { get; set; }
     }
 }
