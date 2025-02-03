8d41786f57c4d1775dcc6fb335af0da68f5f6a17
Jochen Kalmbach (HOMAG) <jochen.kalmbach@homag.com>
2025-02-03 08:48:14 +0100

    Added examples for configuration / added some more propertis in configuration

--- Applications/OrderManager/Contracts/OrderItems/Configuration.cs
+++ Applications/OrderManager/Contracts/OrderItems/Configuration.cs
@@ -15,6 +15,26 @@ public class Configuration : Base
     }
 
     /// <summary>
+    /// An optional library id
+    /// </summary>
+    public string? LibraryId { get; set; }
+
+    /// <summary>
+    /// The id of this configuration module
+    /// </summary>
+    public string? ModuleId { get; set; }
+
+    /// <summary>
+    /// An optional position of this module (x, y, z)
+    /// </summary>
+    public double[]? Position { get; set; }
+
+    /// <summary>
+    /// An optional rotation of this module (x, y, z)
+    /// </summary>
+    public double[]? Rotation { get; set; }
+
+    /// <summary>
     /// Contains configuration attributes.
     /// </summary>
     public IDictionary<string, object>? Attributes { get; set; } = new Dictionary<string, object>(StringComparer.Ordinal);
--- Applications/OrderManager/Tests/GetOrderTests.cs
+++ Applications/OrderManager/Tests/GetOrderTests.cs
@@ -100,5 +100,207 @@ namespace HomagConnect.OrderManager.Tests
 
             order.Trace(nameof(order));
         }
+
+        [TestMethod]
+        public void GetOrderWithConfig()
+        {
+            var order = new Order();
+
+            // Header
+
+            order.OrderNumber = "736362";
+            order.State = OrderState.New;
+            order.OrderName = "Bedroom & bathroom 01";
+            order.Project = "Single family house Müller John";
+            order.PersonInCharge = "Joe";
+            order.OrderDescription = "Lorem ipsum dolor sit amet...";
+            order.OrderDate = DateTime.Today;
+            order.DeliveryDatePlanned = DateTime.Today.AddDays(14);
+
+            order.Link = new Uri($"https://orderManager.homag.cloud/#/subscriptionId/orders/{order.OrderNumber}");
+
+            // Addresses
+            order.Addresses = new Collection<Address>(new List<Address>
+            {
+                new()
+                {
+                    Street = "Musterstraße",
+                    HouseNumber = "1",
+                    PostalCode = "12345",
+                    City = "Musterstadt",
+                    Country = "Deutschland",
+                    Type = AddressType.Delivery | AddressType.Billing
+                }
+            });
+
+            // Customer
+
+            order.CustomerName = "Müller & Co.";
+            order.CustomerNumber = "462642";
+
+            // Details
+
+            order.CreatedAt = DateTimeOffset.Now;
+            order.ChangedAt = DateTimeOffset.Now;
+            order.ChangedBy = "Selfish";
+
+            // Order Items
+
+            order.Items = new()
+            {
+                new Group()
+                {
+                    Id = "9746919d-9611-4d1d-98d3-0fc6f083c1fb",
+                    Items = new()
+                    {
+                        new Configuration
+                        {
+                            Id = "190d9d40-9095-40b0-a7ce-2b85e26b9485",
+                            LibraryId = "CabinetLibrary",
+                            ModuleId = "mr_StorageunitSingle",
+                            Attributes = new Dictionary<string, object>(StringComparer.Ordinal)
+                            {
+                                { "mod_Depth", 548 },
+                                { "mod_Height", 900 },
+                                { "mod_Width", 600 },
+                                { "color", "white" }
+                            },
+                            Items = new()
+                            {
+                                new Configuration
+                                {
+                                    Id = "9746919d-9611-4d1d-98d3-0fc6f083c1fb",
+                                    ModuleId = "mf_Door",
+                                    Attributes = new Dictionary<string, object>(StringComparer.Ordinal)
+                                    {
+                                        { "mod_FrontHeight", 705 }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            order.Trace(nameof(order));
+        }
+
+        [TestMethod]
+        public void GetOrderWithConfigAndParts()
+        {
+            var order = new Order();
+
+            // Header
+
+            order.OrderNumber = "736362";
+            order.State = OrderState.New;
+            order.OrderName = "Bedroom & bathroom 01";
+            order.Project = "Single family house Müller John";
+            order.PersonInCharge = "Joe";
+            order.OrderDescription = "Lorem ipsum dolor sit amet...";
+            order.OrderDate = DateTime.Today;
+            order.DeliveryDatePlanned = DateTime.Today.AddDays(14);
+
+            order.Link = new Uri($"https://orderManager.homag.cloud/#/subscriptionId/orders/{order.OrderNumber}");
+
+            // Addresses
+            order.Addresses = new Collection<Address>(new List<Address>
+            {
+                new()
+                {
+                    Street = "Musterstraße",
+                    HouseNumber = "1",
+                    PostalCode = "12345",
+                    City = "Musterstadt",
+                    Country = "Deutschland",
+                    Type = AddressType.Delivery | AddressType.Billing
+                }
+            });
+
+            // Customer
+
+            order.CustomerName = "Müller & Co.";
+            order.CustomerNumber = "462642";
+
+            // Details
+
+            order.CreatedAt = DateTimeOffset.Now;
+            order.ChangedAt = DateTimeOffset.Now;
+            order.ChangedBy = "Selfish";
+
+            // Order Items
+
+            order.Items = new()
+            {
+                new Group()
+                {
+                    Id = "9746919d-9611-4d1d-98d3-0fc6f083c1fb",
+                    Items = new()
+                    {
+                        new Configuration
+                        {
+                            Id = "190d9d40-9095-40b0-a7ce-2b85e26b9485",
+                            LibraryId = "CabinetLibrary",
+                            ModuleId = "mr_StorageunitSingle",
+                            Attributes = new Dictionary<string, object>(StringComparer.Ordinal)
+                            {
+                                { "mod_Depth", 548 },
+                                { "mod_Height", 900 },
+                                { "mod_Width", 600 },
+                                { "color", "white" }
+                            },
+                            Items = new()
+                            {
+                                new Part
+                                {
+                                    Id = "P 01.01",
+                                    Quantity = 4,
+                                    Description = "Cabinet left",
+                                    Notes = "Lorem ipsum",
+                                    Length = 250,
+                                    Width = 100,
+                                    Thickness = 150
+                                },
+                                new Configuration
+                                {
+                                    Id = "9746919d-9611-4d1d-98d3-0fc6f083c1fb",
+                                    ModuleId = "mf_Door",
+                                    Attributes = new Dictionary<string, object>(StringComparer.Ordinal)
+                                    {
+                                        { "mod_FrontHeight", 705 }
+                                    },
+                                    Items = new()
+                                    {
+                                        new Part
+                                        {
+                                            Id = "P 01.02",
+                                            Quantity = 6,
+                                            Description = "Front panel",
+                                            Notes = "Lorem ipsum",
+                                            Length = 250,
+                                            Width = 100,
+                                            Thickness = 16,
+                                            Items = new()
+                                            {
+                                                new Resource
+                                                {
+                                                    Id = "P 01.02.01",
+                                                    Quantity = 1,
+                                                    Description = "Hinge",
+                                                    Notes = "Lorem ipsum",
+                                                    ArticleNumber = "67840.01"
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            order.Trace(nameof(order));
+        }
     }
 }
