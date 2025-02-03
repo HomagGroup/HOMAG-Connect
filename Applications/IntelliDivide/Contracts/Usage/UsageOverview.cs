4e57f37184b22cd945399a4c025a8aa659a9da54
Matei-Lazar <49794521+Matei-Lazar@users.noreply.github.com>
2025-02-03 10:06:43 +0200

    Update UsageOverview.cs

--- Applications/IntelliDivide/Contracts/Usage/UsageOverview.cs
+++ Applications/IntelliDivide/Contracts/Usage/UsageOverview.cs
@@ -3,21 +3,21 @@ using Newtonsoft.Json;
 
 namespace HomagConnect.IntelliDivide.Contracts.Usage
 {
-	/// <summary>
-	/// Describes the overview per period of the usage of the owned licenses and packages
-	/// </summary>
+    /// <summary>
+    /// Describes the overview per period of the usage of the owned licenses and packages
+    /// </summary>
     public class UsageOverview : UsageBanner
     {
         /// <summary>
-		/// The id of the subscription
-		/// </summary>
-		[JsonProperty(Order = 1)]
-		public Guid SubscriptionId { get; set; }
+	/// The id of the subscription
+	/// </summary>
+	[JsonProperty(Order = 1)]
+	public Guid SubscriptionId { get; set; }
 
-		/// <summary>
-		/// The month of the usage overview
-		/// </summary>
-		[JsonProperty(Order = 2)]
-		public DateTime Period { get; set; }
+	/// <summary>
+	/// The month of the usage overview
+	/// </summary>
+	[JsonProperty(Order = 2)]
+	public DateTime Period { get; set; }
     }
 }
