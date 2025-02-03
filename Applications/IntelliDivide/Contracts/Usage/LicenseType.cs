12dfaa4d7c866cce6d567c165057b527c7a4a915
Matei-Lazar <49794521+Matei-Lazar@users.noreply.github.com>
2025-02-03 10:05:21 +0200

    Update LicenseType.cs

--- Applications/IntelliDivide/Contracts/Usage/LicenseType.cs
+++ Applications/IntelliDivide/Contracts/Usage/LicenseType.cs
@@ -6,17 +6,17 @@ namespace HomagConnect.IntelliDivide.Contracts.Usage
     /// <summary>
     /// Contains the type of the license
     /// </summary>
-	[JsonConverter(typeof(TolerantEnumConverter))]
+    [JsonConverter(typeof(TolerantEnumConverter))]
     public enum LicenseType
     {
-		/// <summary>
-		/// License of type Cutting
-		/// </summary>
-		Cutting,
+	/// <summary>
+	/// License of type Cutting
+	/// </summary>
+	Cutting,
 
-		/// <summary>
-		/// License of type Nesting
-		/// </summary>
-		Nesting
+	/// <summary>
+	/// License of type Nesting
+	/// </summary>
+	Nesting
     }
 }
