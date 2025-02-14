d650bc113d08890f39aaeb31728f276707ac7e87
Boris Wehrle <Boris.Wehrle@homag.com>
2025-02-14 10:39:06 +0100

    References corrected

--- Base/HomagConnect.Base.Tests/Attributes/TemporaryDisabledOnServerAttribute.cs
+++ Base/HomagConnect.Base.Tests/Attributes/TemporaryDisabledOnServerAttribute.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HomagConnect.Base.Tests.Attributes
 {
