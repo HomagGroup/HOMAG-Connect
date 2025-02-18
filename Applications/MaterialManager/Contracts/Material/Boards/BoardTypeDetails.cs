c5403b851c3090293d171d27922ceae7003b5631
Alexandru Marcu <alexandru.marcu@cadrantechnologies.eu>
2025-02-18 16:41:51 +0200

    feat: Add additional data, refactoring

--- Applications/MaterialManager/Contracts/Material/Boards/BoardTypeDetails.cs
+++ Applications/MaterialManager/Contracts/Material/Boards/BoardTypeDetails.cs
@@ -1,33 +1,42 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
+using HomagConnect.Base.Contracts.AdditionalData;
 using HomagConnect.MaterialManager.Contracts.Material.Base;
 
 using Newtonsoft.Json;
 
-namespace HomagConnect.MaterialManager.Contracts.Material.Boards
+namespace HomagConnect.MaterialManager.Contracts.Material.Boards;
+
+/// <summary>
+/// The board type details.
+/// </summary>
+public class BoardTypeDetails : BoardType
 {
     /// <summary>
-    /// The board type details.
+    /// Gets or sets the additional data.
     /// </summary>
-    public class BoardTypeDetails : BoardType
-    {
-        /// <summary>
-        /// Gets or sets the board type allocations.
-        /// </summary>
-        [JsonProperty(Order = 83)]
-        public ICollection<BoardTypeAllocation> Allocations { get; set; } = new List<BoardTypeAllocation>();
+    [JsonProperty(Order = 84)]
+    public Collection<AdditionalDataTexture>? AdditionalData { get; set; }
 
-        /// <summary>
-        /// Gets or sets the list of additional images.
-        /// </summary>
-        [JsonProperty(Order = 81)]
-        public ICollection<ImageInformation> Images { get; set; } =
-            new List<ImageInformation>(); // TODO: Restructure when adding additional data.
+    /// <summary>
+    /// Gets or sets the board type allocations.
+    /// </summary>
+    [JsonProperty(Order = 83)]
+    public ICollection<BoardTypeAllocation> Allocations { get; set; } = new List<BoardTypeAllocation>();
 
-        /// <summary>
-        /// Gets or sets the board type inventory.
-        /// </summary>
-        [JsonProperty(Order = 82)]
-        public ICollection<BoardTypeInventory> Inventory { get; set; } = new List<BoardTypeInventory>();
-    }
+    /// <summary>
+    /// Gets or sets the list of additional images.
+    /// </summary>
+    [JsonProperty(Order = 81)]
+    [Obsolete("This parameter is obsolete. Use AdditionalData instead.")]
+    public ICollection<ImageInformation> Images { get; set; } =
+        new List<ImageInformation>();
+
+    /// <summary>
+    /// Gets or sets the board type inventory.
+    /// </summary>
+    [JsonProperty(Order = 82)]
+    public ICollection<BoardTypeInventory> Inventory { get; set; } = new List<BoardTypeInventory>();
 }
--- Applications/MaterialManager/Contracts/Material/Edgebands/EdgebandTypeDetails.cs
+++ Applications/MaterialManager/Contracts/Material/Edgebands/EdgebandTypeDetails.cs
@@ -1,22 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
+using HomagConnect.Base.Contracts.AdditionalData;
 using HomagConnect.MaterialManager.Contracts.Material.Base;
 
-namespace HomagConnect.MaterialManager.Contracts.Material.Edgebands
+namespace HomagConnect.MaterialManager.Contracts.Material.Edgebands;
+
+/// <summary>
+/// The edgeband type details.
+/// </summary>
+public class EdgebandTypeDetails : EdgebandType
 {
     /// <summary>
-    /// A edgeband type details.
+    /// Gets or sets the additional data.
     /// </summary>
-    public class EdgebandTypeDetails : EdgebandType
-    {
-        /// <summary>
-        /// Gets or sets the board type inventory.
-        /// </summary>
-        public ICollection<EdgebandTypeInventory> Inventory { get; set; } = new List<EdgebandTypeInventory>();
-        
-        /// <summary>
-        /// Gets or sets the list of additional images.
-        /// </summary>
-        public ICollection<ImageInformation> Images { get; set; } = new List<ImageInformation>();
-    }
+    public Collection<AdditionalDataTexture>? AdditionalData { get; set; }
+
+    /// <summary>
+    /// Gets or sets the list of additional images.
+    /// </summary>
+    [Obsolete("This parameter is obsolete. Use AdditionalData instead.")]
+    public ICollection<ImageInformation> Images { get; set; } = new List<ImageInformation>();
+
+    /// <summary>
+    /// Gets or sets the board type inventory.
+    /// </summary>
+    public ICollection<EdgebandTypeInventory> Inventory { get; set; } = new List<EdgebandTypeInventory>();
 }
