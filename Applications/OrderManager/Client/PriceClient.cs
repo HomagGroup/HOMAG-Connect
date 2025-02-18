f909e9d7803dcb55e75e6db3374de1f082017334
Jochen Kalmbach <jochen.kalmbach@homag.com>
2025-02-18 09:16:08 +0100

    Using base method

--- Applications/OrderManager/Client/PriceClient.cs
+++ Applications/OrderManager/Client/PriceClient.cs
@@ -3,7 +3,6 @@ using HomagConnect.Base.Services;
 using HomagConnect.OrderManager.Contracts.Price;
 using Newtonsoft.Json;
 using System;
-using System.Globalization;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -29,15 +28,10 @@ namespace HomagConnect.OrderManager.Client
         /// <inheritdoc />
         public async Task<PriceResponseData> CalculatePrice(PriceRequestData requestData)
         {
-            const string uri = "api/price/calculate";
-            var request = new HttpRequestMessage(HttpMethod.Post, new Uri(uri, UriKind.Relative));
             var txt = JsonConvert.SerializeObject(requestData, SerializerSettings.Default);
             var requestContent = new StringContent(txt, Encoding.UTF8);
             requestContent.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
-            request.Content = requestContent;
-
-            using var response = await Client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
-            response.EnsureSuccessStatusCode();
+            using var response = await PostObject(new Uri("api/price/calculate", UriKind.Relative), requestContent);
             var content = await response.Content.ReadAsStreamAsync();
             using var reader = new StreamReader(content);
             var responseObject = JsonConvert.DeserializeObject<PriceResponseData>(await reader.ReadToEndAsync())!;
