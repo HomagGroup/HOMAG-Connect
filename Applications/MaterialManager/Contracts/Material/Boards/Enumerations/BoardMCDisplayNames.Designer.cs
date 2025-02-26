4585a1af6dc9f91a7eea282c05fd7a6da42128e6
joBueche <aa@b.de>
2025-02-26 07:59:38 +0100

    shorten even more

--- Applications/MaterialManager/Contracts/Material/Boards/Enumerations/BoardMCDisplayNames.Designer.cs
+++ Applications/MaterialManager/Contracts/Material/Boards/Enumerations/BoardMCDisplayNames.Designer.cs
@@ -22,14 +22,14 @@ namespace HomagConnect.MaterialManager.Contracts.Material.Boards.Enumerations {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class BoardMatCatDisplayNames {
+    internal class BoardMCDisplayNames {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal BoardMatCatDisplayNames() {
+        internal BoardMCDisplayNames() {
         }
         
         /// <summary>
@@ -39,8 +39,8 @@ namespace HomagConnect.MaterialManager.Contracts.Material.Boards.Enumerations {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HomagConnect.MaterialManager.Contracts.Material.Boards.Enumerations.BoardMatCatDi" +
-                            "splayNames", typeof(BoardMatCatDisplayNames).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HomagConnect.MaterialManager.Contracts.Material.Boards.Enumerations.BoardMCDispla" +
+                            "yNames", typeof(BoardMCDisplayNames).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- Applications/MaterialManager/Contracts/Material/Boards/Enumerations/BoardMaterialCategory.cs
+++ Applications/MaterialManager/Contracts/Material/Boards/Enumerations/BoardMaterialCategory.cs
@@ -7,7 +7,7 @@ namespace HomagConnect.MaterialManager.Contracts.Material.Boards.Enumerations
     /// <summary>
     /// Board material category
     /// </summary>
-    [ResourceManager(typeof(BoardMatCatDisplayNames))]
+    [ResourceManager(typeof(BoardMCDisplayNames))]
     public enum BoardMaterialCategory
     {
         // ReSharper disable InconsistentNaming
