a903ebe3391bc2544645acf35e7a82ea9703d987
Katzenberger, Robin <Robin.Katzenberger@homag.com>
2025-03-18 15:02:37 +0100

    refactor: run resharper and delete duplicated method GetOrderWithConfigAndParts

--- Applications/OrderManager/Tests/Orders/GetOrderTests.cs
+++ Applications/OrderManager/Tests/Orders/GetOrderTests.cs
@@ -72,6 +72,8 @@ namespace HomagConnect.OrderManager.Tests.Orders
                 new Group()
                 {
                     Id = "9746919d-9611-4d1d-98d3-0fc6f083c1fb",
+                    Name = "Test",
+                    Source = "OrderConfigurator",
                     Items = new()
                     {
                         new ConfigurationPosition
@@ -542,131 +544,6 @@ namespace HomagConnect.OrderManager.Tests.Orders
                                         new ConfigurationAttribute("mod_Width", 1000),
                                         new ConfigurationAttribute("mod_TypeElement", "TallUnit")
                                     },
-                                }
-                            }
-                        }
-                    }
-                }
-            };
-
-            order.Trace(nameof(order));
-
-            // Try to serialize and deserialize it
-            var json = JsonConvert.SerializeObject(order, SerializerSettings.Default);
-            var o = JsonConvert.DeserializeObject<OrderDetails>(json, SerializerSettings.Default);
-            o.Should().BeEquivalentTo(order);
-        }
-
-        [TestMethod]
-        public void GetOrderWithConfigAndParts()
-        {
-            var order = new OrderDetails();
-
-            // Header
-
-            order.OrderNumber = "736362";
-            order.State = OrderState.New;
-            order.OrderName = "Bedroom & bathroom 01";
-            order.Project = "Single family house Müller John";
-            order.PersonInCharge = "Joe";
-            order.OrderDescription = "Lorem ipsum dolor sit amet...";
-            order.OrderDate = DateTime.Today;
-            order.DeliveryDatePlanned = DateTime.Today.AddDays(14);
-
-            order.Link = new Uri($"https://orderManager.homag.cloud/#/subscriptionId/orders/{order.OrderNumber}");
-
-            // Addresses
-            order.Addresses = new Collection<Address>(new List<Address>
-            {
-                new()
-                {
-                    Street = "Musterstraße",
-                    HouseNumber = "1",
-                    PostalCode = "12345",
-                    City = "Musterstadt",
-                    Country = "Deutschland",
-                    Type = AddressType.Delivery | AddressType.Billing
-                }
-            });
-
-            // Customer
-
-            order.CustomerName = "Müller & Co.";
-            order.CustomerNumber = "462642";
-
-            // Details
-
-            order.CreatedAt = DateTimeOffset.Now;
-            order.ChangedAt = DateTimeOffset.Now;
-            order.ChangedBy = "Selfish";
-
-            // Order Items
-
-            order.Items = new()
-            {
-                new Group()
-                {
-                    Id = "9746919d-9611-4d1d-98d3-0fc6f083c1fb",
-                    Name = "Test",
-                    Source = "OrderConfigurator",
-                    Items = new()
-                    {
-                        new ConfigurationPosition
-                        {
-                            Id = "190d9d40-9095-40b0-a7ce-2b85e26b9485",
-                            LibraryId = "CabinetLibrary",
-                            ModuleId = "mr_StorageunitSingle",
-                            Attributes = new ()
-                            {
-                                new ConfigurationAttribute("mod_Depth", 548),
-                                new ConfigurationAttribute("mod_Height", 900),
-                                new ConfigurationAttribute("mod_Width", 600),
-                                new ConfigurationAttribute("color", "white")
-                            },
-                            Items = new()
-                            {
-                                new Part
-                                {
-                                    Id = "P 01.01",
-                                    Quantity = 4,
-                                    Description = "Cabinet left",
-                                    Notes = "Lorem ipsum",
-                                    Length = 250,
-                                    Width = 100,
-                                    Thickness = 150
-                                },
-                                new Configuration
-                                {
-                                    Id = "9746919d-9611-4d1d-98d3-0fc6f083c1fb",
-                                    ModuleId = "mf_Door",
-                                    Attributes = new ()
-                                    {
-                                        new ConfigurationAttribute("mod_FrontHeight", 705)
-                                    },
-                                    Items = new()
-                                    {
-                                        new Part
-                                        {
-                                            Id = "P 01.02",
-                                            Quantity = 6,
-                                            Description = "Front panel",
-                                            Notes = "Lorem ipsum",
-                                            Length = 250,
-                                            Width = 100,
-                                            Thickness = 16,
-                                            Items = new()
-                                            {
-                                                new Resource
-                                                {
-                                                    Id = "P 01.02.01",
-                                                    Quantity = 1,
-                                                    Description = "Hinge",
-                                                    Notes = "Lorem ipsum",
-                                                    ArticleNumber = "67840.01"
-                                                }
-                                            }
-                                        }
-                                    }
                                 }
                             }
                         }
