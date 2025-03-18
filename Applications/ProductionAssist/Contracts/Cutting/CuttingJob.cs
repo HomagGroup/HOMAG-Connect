c9eac854e61cde84e2672c8aabfb06e364763b00
Katzenberger, Robin <Robin.Katzenberger@homag.com>
2025-03-18 15:26:32 +0100

    feat: adapt model

--- Applications/ProductionAssist/Contracts/Cutting/CuttingJob.cs
+++ Applications/ProductionAssist/Contracts/Cutting/CuttingJob.cs
@@ -2,20 +2,54 @@
 
 namespace HomagConnect.ProductionAssist.Contracts.Cutting
 {
+    /// <summary>
+    /// Cutting job
+    /// </summary>
     public class CuttingJob
     {
+        /// <summary>
+        /// Available boards
+        /// </summary>
         public string AvailableBoards { get; set; }
 
-        public string Boards { get; set; }
-
+        /// <summary>
+        /// Cutting job id
+        /// </summary>
         public Guid Id { get; set; }
 
+        /// <summary>
+        /// Materials
+        /// </summary>
         public string Materials { get; set; }
 
+        /// <summary>
+        /// Cutting job name
+        /// </summary>
         public string Name { get; set; }
 
-        public string Parts { get; set; }
+        /// <summary>
+        /// Produced parts
+        /// </summary>
+        public string ProducedParts { get; set; }
 
+        /// <summary>
+        /// Required boards
+        /// </summary>
+        public string RequiredBoards { get; set; }
+
+        /// <summary>
+        /// Required parts
+        /// </summary>
+        public string RequiredParts { get; set; }
+
+        /// <summary>
+        /// Production order id
+        /// </summary>
         public CuttingState State { get; set; }
+
+        /// <summary>
+        /// Used boards
+        /// </summary>
+        public string UsedBoards { get; set; }
     }
 }
