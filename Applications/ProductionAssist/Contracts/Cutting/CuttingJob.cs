2d42922716b87732e7b724204e67e2a030eca3c2
Katzenberger, Robin <Robin.Katzenberger@homag.com>
2025-03-18 15:21:04 +0100

    feat: add default CuttingJob model to pA client

---
+++ Applications/ProductionAssist/Contracts/Cutting/CuttingJob.cs
@@ -0,0 +1,21 @@
+﻿using HomagConnect.ProductionAssist.Contracts.Cutting.Enumerations;
+
+namespace HomagConnect.ProductionAssist.Contracts.Cutting
+{
+    public class CuttingJob
+    {
+        public string AvailableBoards { get; set; }
+
+        public string Boards { get; set; }
+
+        public Guid Id { get; set; }
+
+        public string Materials { get; set; }
+
+        public string Name { get; set; }
+
+        public string Parts { get; set; }
+
+        public CuttingState State { get; set; }
+    }
+}
