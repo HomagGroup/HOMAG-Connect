71e452cbb530139f39fbabf48cd8786f79bea38c
Robin Katzenberger <141736258+HOMAG-Robin-Katzenberger@users.noreply.github.com>
2025-03-20 13:02:55 +0100

    feat: add default CuttingJob model to pA client (#273)
    * feat: add default CuttingJob model to pA client
    
    * feat: adapt model
    
    * feat: change dataTypes of boards and parts

---
+++ Applications/ProductionAssist/Contracts/Cutting/Enumerations/CuttingState.cs
@@ -0,0 +1,33 @@
+﻿using HomagConnect.Base.Contracts.Converter;
+
+using Newtonsoft.Json;
+
+namespace HomagConnect.ProductionAssist.Contracts.Cutting.Enumerations
+{
+    /// <summary>
+    /// Cutting State
+    /// </summary>
+    [JsonConverter(typeof(TolerantEnumConverter))]
+    public enum CuttingState
+    {
+        /// <summary>
+        /// Not yet started
+        /// </summary>
+        NotStarted,
+
+        /// <summary>
+        /// Started (at least one part produced)
+        /// </summary>
+        Started,
+
+        /// <summary>
+        /// Finished (all parts produced)
+        /// </summary>
+        Finished,
+
+        /// <summary>
+        /// Archived
+        /// </summary>
+        Archived
+    }
+}
