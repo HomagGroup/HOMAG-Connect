b53589598f37647abeaee5dc1b233abb9d559de5
Robin Katzenberger <robin.katzenberger@homag.com>
2025-03-28 12:06:09 +0100

    feat: add get and delete functionality for pA cutting and nesting to client and interface

--- Applications/ProductionAssist/Contracts/IProductionAssistFeedbackClient.cs
+++ Applications/ProductionAssist/Contracts/IProductionAssistFeedbackClient.cs
@@ -1,4 +1,6 @@
-﻿using HomagConnect.ProductionAssist.Contracts.Feedback;
+﻿using HomagConnect.ProductionAssist.Contracts.Cutting;
+using HomagConnect.ProductionAssist.Contracts.Feedback;
+using HomagConnect.ProductionAssist.Contracts.Nesting;
 
 namespace HomagConnect.ProductionAssist.Contracts;
 
@@ -17,4 +19,63 @@ public interface IProductionAssistFeedbackClient
     /// Report a production entity as finished.
     /// </summary>
     Task ReportAsFinished(Guid workstationId, string identification, int quantity, DateTimeOffset? timestamp);
+
+    #region Cutting
+
+    /// <summary>
+    /// Retrieve a cutting job by id.
+    /// </summary>
+    Task<CuttingJob> GetCuttingJob(Guid cuttingJobId);
+
+    /// <summary>
+    /// Retrieve the list of all cutting jobs.
+    /// </summary>
+    /// <returns></returns>
+    Task<IEnumerable<CuttingJob>> GetCuttingJobs();
+
+    /// <summary>
+    /// Retrieve cutting jobs by id.
+    /// </summary>
+    Task<IEnumerable<CuttingJob>> GetCuttingJobs(Guid[] cuttingJobIds);
+
+    /// <summary>
+    /// Delete a cutting job.
+    /// </summary>
+    Task DeleteCuttingJob(Guid cuttingJobId);
+
+    /// <summary>
+    /// Delete cutting jobs
+    /// </summary>
+    Task DeleteCuttingJobs(Guid[] cuttingJobIds);
+
+    #endregion Cutting
+
+    #region Nesting
+
+    /// <summary>
+    /// Retrieve a nesting job by id.
+    /// </summary>
+    Task<NestingJob> GetNestingJob(Guid nestingJobId);
+
+    /// <summary>
+    /// Retrieve the list of all nesting jobs.
+    /// </summary>
+    Task<IEnumerable<NestingJob>> GetNestingJobs();
+
+    /// <summary>
+    /// Retrieve nesting jobs by id.
+    /// </summary>
+    Task<IEnumerable<NestingJob>> GetNestingJobs(Guid[] nestingJobIds);
+
+    /// <summary>
+    /// Delete a nesting job.
+    /// </summary>
+    Task DeleteNestingJob(Guid nestingJobId);
+
+    /// <summary>
+    /// Delete nesting jobs
+    /// </summary>
+    Task DeleteNestingJobs(Guid[] nestingJobIds);
+
+    #endregion Nesting
 }
