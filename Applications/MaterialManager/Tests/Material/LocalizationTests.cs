cbefda912a8588dd9eb4f28f983e7371f6bb2165
Boris Wehrle (HOMAG) <141398900+HOMAG-Boris-Wehrle@users.noreply.github.com>
2025-04-04 08:28:58 +0200

    EnumExtensions made more generic

--- Applications/MaterialManager/Tests/Material/LocalizationTests.cs
+++ Applications/MaterialManager/Tests/Material/LocalizationTests.cs
@@ -2,7 +2,6 @@
 
 using HomagConnect.Base.Contracts.Enumerations;
 using HomagConnect.Base.Extensions;
-using HomagConnect.Base.TestBase.Attributes;
 using HomagConnect.MaterialManager.Contracts.Material.Boards.Enumerations;
 
 namespace HomagConnect.MaterialManager.Tests.Material
@@ -14,25 +13,24 @@ namespace HomagConnect.MaterialManager.Tests.Material
     {
         /// <summary />
         [TestMethod]
-        public void MaterialManager_Localization_Grain()
-        {
-            var displayNames = EnumExtensions.GetDisplayNames<Grain>(CultureInfo.GetCultureInfo("de"));
-
-            Assert.AreEqual(3, displayNames.Count);
-            Assert.AreEqual("Keine", displayNames[Grain.None]);
-
-            displayNames.Trace();
-        }
-
-        /// <summary />
-        [TestMethod]
-        [TemporaryDisabledOnServer(2025, 3, 10, "DF-Foundation")]
         public void MaterialManager_Localization_BoardMaterialCategory()
         {
             var displayNames = EnumExtensions.GetDisplayNames<BoardMaterialCategory>(CultureInfo.GetCultureInfo("de"));
 
             Assert.IsTrue(displayNames.Count > 0);
             Assert.AreEqual("Spanplatte", displayNames[BoardMaterialCategory.Chipboard]);
+
+            displayNames.Trace();
+        }
+
+        /// <summary />
+        [TestMethod]
+        public void MaterialManager_Localization_Grain()
+        {
+            var displayNames = EnumExtensions.GetDisplayNames<Grain>(CultureInfo.GetCultureInfo("de"));
+
+            Assert.AreEqual(3, displayNames.Count);
+            Assert.AreEqual("Keine", displayNames[Grain.None]);
 
             displayNames.Trace();
         }
--- Base/HomagConnect.Base/Extensions/EnumExtensions.cs
+++ Base/HomagConnect.Base/Extensions/EnumExtensions.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
-using System.Linq;
 using System.Reflection;
 using System.Resources;
 
@@ -12,28 +11,42 @@ namespace HomagConnect.Base.Extensions
 {
     public static class EnumExtensions
     {
+        #region Public Methods
+
         /// <summary>
         /// Gets the display names of all enum values in the specified culture.
         /// </summary>
         public static IDictionary<T, string> GetDisplayNames<T>(CultureInfo culture) where T : Enum
         {
             var enumType = typeof(T);
-            var enumValues = Enum.GetValues(enumType);
             var dictionary = new Dictionary<T, string>();
+
+            foreach (var keyValuePair in GetDisplayNames(enumType, culture))
+            {
+                dictionary.Add((T)keyValuePair.Key, keyValuePair.Value);
+            }
+
+            return dictionary;
+        }
+
+        public static Dictionary<object, string> GetDisplayNames(Type enumType, CultureInfo culture)
+        {
+            var enumValues = Enum.GetValues(enumType);
+            var dictionary = new Dictionary<object, string>();
             var resourceManager = GetResourceManager(enumType);
 
-            foreach (var enumValue in enumValues.OfType<T>())
+            foreach (var enumValue in enumValues)
             {
                 var enumName = Enum.GetName(enumType, enumValue);
                 var enumDisplayName = enumValue.ToString();
 
                 if (!string.IsNullOrWhiteSpace(enumName))
                 {
-                    if (enumValue.TryGetDisplayNameFromResourceManager(resourceManager, culture, out var resourceManagerDisplayName))
+                    if (TryGetDisplayNameFromResourceManager(enumValue, enumType, resourceManager, culture, out var resourceManagerDisplayName))
                     {
                         enumDisplayName = resourceManagerDisplayName;
                     }
-                    else if (enumValue.TryGetDisplayNameFromDisplayAttribute(out var enumDisplayAttributeName))
+                    else if (TryGetDisplayNameFromDisplayAttribute(enumValue, enumType, out var enumDisplayAttributeName))
                     {
                         enumDisplayName = enumDisplayAttributeName;
                     }
@@ -43,21 +56,55 @@ namespace HomagConnect.Base.Extensions
                     }
                 }
 
-                dictionary.Add(enumValue, enumDisplayName ?? enumValue.ToString());
+                if (!string.IsNullOrWhiteSpace(enumDisplayName))
+                {
+                    dictionary.Add(enumValue, CapitalizeFirstLetter(enumDisplayName));
+                }
             }
 
             return dictionary;
         }
 
-        private static bool TryGetDisplayNameFromDisplayAttribute<T>(this T enumValue, out string? enumDisplayName) where T : Enum
+        #endregion
+
+        #region Private Methods
+
+        private static bool TryGetDisplayNameFromResourceManager(object enumValue, Type type, ResourceManager? resourceManager, CultureInfo cultureInfo, out string? enumDisplayName)
         {
             enumDisplayName = string.Empty;
 
-            var enumName = Enum.GetName(typeof(T), enumValue);
+            if (resourceManager != null)
+            {
+                var enumName = Enum.GetName(type, enumValue);
+
+                if (!string.IsNullOrWhiteSpace(enumName))
+                {
+                    enumDisplayName = resourceManager.GetString(enumName, cultureInfo) ?? string.Empty;
+
+                    if (!string.IsNullOrWhiteSpace(enumDisplayName))
+                    {
+                        return true;
+                    }
+                }
+
+                if (!string.IsNullOrWhiteSpace(enumDisplayName))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool TryGetDisplayNameFromDisplayAttribute(object enumValue, Type type, out string? enumDisplayName)
+        {
+            enumDisplayName = string.Empty;
+
+            var enumName = Enum.GetName(type, enumValue);
 
             if (!string.IsNullOrWhiteSpace(enumName))
             {
-                var field = typeof(T).GetField(enumName);
+                var field = type.GetField(enumName);
 
                 if (field != null)
                 {
@@ -85,42 +132,13 @@ namespace HomagConnect.Base.Extensions
             return false;
         }
 
-        private static bool TryGetDisplayNameFromResourceManager<T>(this T enumValue, ResourceManager? resourceManager, CultureInfo cultureInfo, out string? enumDisplayName) where T : Enum
-        {
-            enumDisplayName = string.Empty;
-
-            if (resourceManager != null)
-            {
-                var enumName = Enum.GetName(typeof(T), enumValue);
-
-                if (!string.IsNullOrWhiteSpace(enumName))
-                {
-                    enumDisplayName = resourceManager.GetString(enumName, cultureInfo) ?? string.Empty;
-
-                    if (!string.IsNullOrWhiteSpace(enumDisplayName))
-                    {
-                        enumDisplayName = CapitalizeFirstLetter(enumDisplayName);
-
-                        return true;
-                    }
-                }
-
-                if (!string.IsNullOrWhiteSpace(enumDisplayName))
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
         private static string CapitalizeFirstLetter(string input)
         {
             if (string.IsNullOrEmpty(input))
             {
                 return string.Empty;
             }
-        
+
             var stringArray = input.ToCharArray();
 
             if (char.IsLower(stringArray[0]))
@@ -137,5 +155,7 @@ namespace HomagConnect.Base.Extensions
 
             return resourceManagerAttribute != null ? new ResourceManager(resourceManagerAttribute.ResourceManagerType) : null;
         }
+
+        #endregion
     }
 }
