1ce513393585ac8ff6962d391b1f8d7d8642c598
Wehrle, Boris <Boris.Wehrle@homag.com>
2025-05-08 10:24:55 +0200

    Frequently used extension methods added.

--- Base/HomagConnect.Base.Contracts/Extensions/FileInfoExtensions.cs
+++ Base/HomagConnect.Base.Contracts/Extensions/FileInfoExtensions.cs
@@ -16,11 +16,29 @@ namespace HomagConnect.Base.Contracts.Extensions
         }
 
         /// <summary>
+        /// Gets the file name without extension of a file.
+        /// </summary>
+        public static string GetFileNameWithoutExtension(this FileInfo fileInfo)
+        {
+            return Path.GetFileNameWithoutExtension(fileInfo.Name);
+        }
+
+        /// <summary>
         /// Gets the mimetype of a file based on its extension.
         /// </summary>
         public static string GetMimeType(this FileInfo fileInfo)
         {
             return MimeTypes.GetMimeType(fileInfo);
+        }
+
+        /// <summary>
+        /// Reads the content of a file.
+        /// </summary>
+        public static string ReadFileContent(this FileInfo fileInfo)
+        {
+            using var reader = fileInfo.OpenText();
+
+            return reader.ReadToEnd();
         }
     }
 }
