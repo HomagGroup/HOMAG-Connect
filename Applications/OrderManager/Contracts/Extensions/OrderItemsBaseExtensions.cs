6711b537f142ffe6dec8d7eeb6503b1f06279710
Boris Wehrle (HOMAG) <141398900+HOMAG-Boris-Wehrle@users.noreply.github.com>
2025-05-08 15:30:15 +0200

    Frequently used extension methods added. (#322)
    * Frequently used extension methods added.
    
    * .Net Standard 2.0 issue fixed
    
    * Failing test deactivated

---
+++ Applications/OrderManager/Contracts/Extensions/OrderItemsBaseExtensions.cs
@@ -0,0 +1,37 @@
+﻿using HomagConnect.Base.Contracts.AdditionalData;
+
+namespace HomagConnect.OrderManager.Contracts.Extensions;
+
+/// <summary>
+/// Extensions for <see cref="OrderItems.Base" />.
+/// </summary>
+public static class OrderItemsBaseExtensions
+{
+    /// <summary>
+    /// Gets the additional data entities from the order item and contained order items.
+    /// </summary>
+    public static IEnumerable<AdditionalDataEntity> GetAdditionalDataEntities(this OrderItems.Base? item)
+    {
+        if (item != null)
+        {
+            if (item.AdditionalData != null)
+            {
+                foreach (var additionalData in item.AdditionalData)
+                {
+                    yield return additionalData;
+                }
+            }
+
+            if (item.Items != null)
+            {
+                foreach (var itemItem in item.Items)
+                {
+                    foreach (var additionalData in GetAdditionalDataEntities(itemItem))
+                    {
+                        yield return additionalData;
+                    }
+                }
+            }
+        }
+    }
+}
