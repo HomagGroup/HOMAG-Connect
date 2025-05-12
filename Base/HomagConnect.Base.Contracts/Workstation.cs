514d9031cb956af95d7d58b6606fc19aac96f642
HOMAG-Alexandru-Marcu <alexandru.marcu@cadrantechnologies.eu>
2025-05-12 11:53:31 +0300

    Feat/320540 add workstation dto (#324)
    * feat: add DTO, add enum
    
    * feat: add translations
    
    * fix: remove unsupported required keyword

---
+++ Base/HomagConnect.Base.Contracts/Workstation.cs
@@ -0,0 +1,24 @@
+﻿using HomagConnect.Base.Contracts.Enumerations;
+
+namespace HomagConnect.Base.Contracts;
+
+/// <summary>
+/// Defines the workstation.
+/// </summary>
+public class Workstation
+{
+    /// <summary>
+    /// Gets or sets the Id.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the workstation type.
+    /// </summary>
+    public WorkstationType WorkstationType { get; set; }
+}
