0b304c4a6f39a50a674a455a08d7541f49d5bdc8
Alexandru Marcu <alexandru.marcu@cadrantechnologies.eu>
2025-05-12 10:29:16 +0300

    fix: remove unsupported required keyword

--- Base/HomagConnect.Base.Contracts/Workstation.cs
+++ Base/HomagConnect.Base.Contracts/Workstation.cs
@@ -10,15 +10,15 @@ public class Workstation
     /// <summary>
     /// Gets or sets the Id.
     /// </summary>
-    public required Guid Id { get; set; }
+    public Guid Id { get; set; }
 
     /// <summary>
     /// Gets or sets the name
     /// </summary>
-    public required string Name { get; set; }
+    public string Name { get; set; }
 
     /// <summary>
     /// Gets or sets the workstation type.
     /// </summary>
-    public required WorkstationType WorkstationType { get; set; }
+    public WorkstationType WorkstationType { get; set; }
 }
