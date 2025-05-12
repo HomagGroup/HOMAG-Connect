1e8fae5e0730df4cf66d1be3024c4c5bc23bcf1a
Alexandru Marcu <alexandru.marcu@cadrantechnologies.eu>
2025-05-12 09:14:42 +0300

    feat: add DTO, add enum

---
+++ Base/HomagConnect.Base.Contracts/Enumerations/WorkstationType.cs
@@ -0,0 +1,22 @@
+﻿using HomagConnect.Base.Contracts.Converter;
+
+using Newtonsoft.Json;
+
+namespace HomagConnect.Base.Contracts.Enumerations;
+
+/// <summary>
+/// The type of the workstation.
+/// </summary>
+[JsonConverter(typeof(TolerantEnumConverter))]
+public enum WorkstationType
+{
+    /// <summary>
+    /// The workstation is a Boards one
+    /// </summary>
+    Boards,
+
+    /// <summary>
+    /// The workstation is an Edgebanding one
+    /// </summary>
+    Edgebanding
+}
---
+++ Base/HomagConnect.Base.Contracts/Workstation.cs
@@ -0,0 +1,24 @@
+﻿using HomagConnect.Base.Contracts.Enumerations;
+
+namespace HomagConnect.Base.Contracts;
+
+/// <summary>
+/// Defines the workstation.
+/// </summary>
+public class Workstation
+{
+    /// <summary>
+    /// Gets or sets the Id.
+    /// </summary>
+    public required Guid Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name
+    /// </summary>
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the workstation type.
+    /// </summary>
+    public required WorkstationType WorkstationType { get; set; }
+}
