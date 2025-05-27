23f6bbe57057bba701ebdfc3c660d036646cfd2b
Boris Wehrle (HOMAG) <141398900+HOMAG-Boris-Wehrle@users.noreply.github.com>
2025-05-27 14:15:22 +0200

    Draft productionAssist GetOrderItems.

---
+++ Applications/ProductionAssist/Contracts/IProductionAssistClient.cs
@@ -0,0 +1,23 @@
+﻿#nullable enable
+using HomagConnect.ProductionManager.Contracts.ProductionEntity;
+
+namespace HomagConnect.ProductionAssist.Contracts
+{
+    /// <summary>
+    /// productionAssist Client
+    /// </summary>
+    public interface IProductionAssistClient
+    {
+        /// <summary>
+        /// Gets the <see cref="ProductionEntity" /> for a given <see cref="ProductionEntity.Id" /> id or
+        /// <see cref="ProductionEntity.Barcode" /> which belongs to any not archived order.
+        /// </summary>
+        Task<ProductionEntity?> GetOrderItem(string identifier);
+
+        /// <summary>
+        /// Gets the <see cref="ProductionEntity" />s for given <see cref="ProductionEntity.Id" />s or
+        /// <see cref="ProductionEntity.Barcode" />s which belongs to any not archived order.
+        /// </summary>
+        Task<ProductionEntity[]?> GetOrderItems(string[] identifiers);
+    }
+}
