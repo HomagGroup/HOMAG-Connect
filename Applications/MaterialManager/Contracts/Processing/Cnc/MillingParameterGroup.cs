2d5172ef6e915db182d7353ce793a2b42ca33c9d
Liehner, Adrian <Adrian.Liehner@homag.com>
2025-06-04 13:03:45 +0200

    feat: improve millingParameterGroup

--- Applications/MaterialManager/Contracts/Processing/Cnc/MillingParameterGroup.cs
+++ Applications/MaterialManager/Contracts/Processing/Cnc/MillingParameterGroup.cs
@@ -4,6 +4,9 @@ using Newtonsoft.Json;
 
 namespace HomagConnect.MaterialManager.Contracts.Processing.Cnc
 {
+    /// <summary>
+    /// The milling parameter group.
+    /// </summary>
     public class MillingParameterGroup
     {
         /// <summary>
@@ -11,20 +14,20 @@ namespace HomagConnect.MaterialManager.Contracts.Processing.Cnc
         /// </summary>
         [JsonProperty(Order = 1)]
         [Required]
-        public string MaterialGroupName { get; set; }
+        public required string MaterialGroupName { get; set; }
 
         /// <summary>
         /// The parameters of the group.
         /// </summary>
         [JsonProperty(Order = 3)]
         [Required]
-        public MillingParameters Parameters { get; set; }
+        public MillingParameters Parameters { get; set; } = new();
 
         /// <summary>
         /// The name of the tool group.
         /// </summary>
         [JsonProperty(Order = 2)]
         [Required]
-        public string ToolGroupName { get; set; }
+        public required string ToolGroupName { get; set; }
     }
 }
