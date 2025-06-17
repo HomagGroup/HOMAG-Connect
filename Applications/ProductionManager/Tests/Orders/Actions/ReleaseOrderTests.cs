2718de25887fb26b066458822cadbedc5c1f824e
ACCESA\fabian.ardeiu <fabian.ardeiu@accesa.eu>
2025-06-17 12:35:42 +0300

    Added release/reset release order

---
+++ Applications/ProductionManager/Tests/Orders/Actions/ReleaseOrderTests.cs
@@ -0,0 +1,44 @@
+﻿using HomagConnect.ProductionManager.Samples.Orders.Actions;
+
+namespace HomagConnect.ProductionManager.Tests.Orders.Actions
+{
+    [TestClass]
+    [TestCategory("ProductionManager")]
+    [TestCategory("ProductionManager.Orders")]
+    public class ReleaseOrderTests : ProductionManagerTestBase
+    {
+        [TestMethod]
+        public async Task ReleaseOrder()
+        {
+            // Arranage
+            var productionManagerClient = GetProductionManagerClient();
+
+            // Act
+            try
+            {
+                await ReleaseOrderSamples.ReleaseOrder(productionManagerClient);
+            }
+            catch (ArgumentNullException)
+            {
+                // catch as no hardcoded id is set in sample
+            }
+        }
+
+        [TestMethod]
+        public async Task ResetReleaseOrder()
+        {
+            // Arranage
+            var productionManagerClient = GetProductionManagerClient();
+
+            // Act
+            try
+            {
+                await ReleaseOrderSamples.ResetReleaseOrder(productionManagerClient);
+            }
+            catch (ArgumentNullException)
+            {
+                // catch as no hardcoded id is set in sample
+            }
+        }
+    }
+}
