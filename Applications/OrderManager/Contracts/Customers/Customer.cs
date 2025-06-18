81ba3891e9af31ab20c8382d352ac62dca8464a8
Katzenberger, Robin <Robin.Katzenberger@homag.com>
2025-06-18 10:22:47 +0200

    feat: add customer model to orderManager client

--- Applications/OrderManager/Contracts/Customers/Customer.cs
+++ Applications/OrderManager/Contracts/Customers/Customer.cs
@@ -1,10 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.ObjectModel;
+
+using HomagConnect.Base.Contracts.AdditionalData;
 
 namespace HomagConnect.OrderManager.Contracts.Customers
 {
-    internal class Customer
+    /// <summary>
+    /// Customer data
+    /// </summary>
+    public class Customer
     {
+        /// <summary>
+        /// Gets or sets the additional data.
+        /// </summary>
+        public Collection<AdditionalDataEntity>? AdditionalData { get; set; }
+
+        /// <summary>
+        /// Gets or sets the API access status.
+        /// </summary>
+        public bool ApiAccess { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the customer.
+        /// </summary>
+        public string? Company { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address of the customer.
+        /// </summary>
+        public string? CustomerNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the contact email of the customer.
+        /// </summary>
+        public string? Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the unique identifier of the customer.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the contact phone number of the customer.
+        /// </summary>
+        public string? Notes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the contact telephone number of the customer.
+        /// </summary>
+        public string? TelephoneNumber { get; set; }
     }
 }
