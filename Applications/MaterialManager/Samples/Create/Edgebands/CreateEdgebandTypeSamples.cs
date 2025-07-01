ceb0ad35be5887729063939c127aea644f65019d
Nicole Helfenstein <nicole.helfenstein@homag.com>
2025-07-01 10:03:55 +0200

    new samples und a correction in the readme for materials edgeband

--- Applications/MaterialManager/Samples/Create/Edgebands/CreateEdgebandTypeSamples.cs
+++ Applications/MaterialManager/Samples/Create/Edgebands/CreateEdgebandTypeSamples.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using HomagConnect.Base.Extensions;
+using HomagConnect.MaterialManager.Contracts.Material.Edgebands.Enumerations;
+using HomagConnect.MaterialManager.Contracts.Material.Edgebands.Interfaces;
+using HomagConnect.MaterialManager.Contracts.Request;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,48 @@ using System.Threading.Tasks;
 
 namespace HomagConnect.MaterialManager.Samples.Create.Edgebands
 {
-    internal class CreateEdgebandTypeSamples
+    public class CreateEdgebandTypeSamples
     {
+        /// <summary>
+        /// The example shows how to create a edgeband type.
+        /// </summary>
+        public static async Task Edgeband_CreateEdgebandType(IMaterialManagerClientMaterialEdgebands materialManager)
+        {
+            var edgebandTypeRequest = new MaterialManagerRequestEdgebandType
+            {
+                EdgebandCode = "EB_White_1mm",
+                Height = 20,
+                Thickness = 1.0,
+                DefaultLength = 23.0,
+                MaterialCategory = EdgebandMaterialCategory.Veneer,
+                Process = EdgebandingProcess.Other,
+            };
+            var newEdgebandType = await materialManager.CreateEdgebandType(edgebandTypeRequest);
+            newEdgebandType.Trace();
+        }
+
+        /// <summary>
+        /// The example shows how to create a edgeband type with technology macro.
+        /// </summary>
+        public static async Task Edgeband_UpdateEdgebandType(IMaterialManagerClientMaterialEdgebands materialManager)
+        {
+            var edgebandTypeRequest = new MaterialManagerRequestEdgebandType
+            {
+                EdgebandCode = "EB_White_1mm",
+                Height = 20,
+                Thickness = 1.0,
+                DefaultLength = 23.0,
+                MaterialCategory = EdgebandMaterialCategory.Veneer,
+                Process = EdgebandingProcess.Other,
+                MachineTechnologyMacro = new Dictionary<string, string>
+            {
+                { "hg0000000000", "ABS_1.00_RM_HM"}
+            },
+                // other properties
+            };
+            var newEdgebandType = await materialManager.CreateEdgebandType(edgebandTypeRequest);
+            newEdgebandType.Trace();
+        }
     }
+
 }
