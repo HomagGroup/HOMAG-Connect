52dff9fd0785e8c86770d9e1faa0cf380273988a
Peter <peter.lorincz@principal33.com>
2025-07-01 09:56:03 +0300

    make public for tests

--- Applications/MaterialManager/Contracts/Processing/Optimization/BookHeightModeDisplayNames.Designer.cs
+++ Applications/MaterialManager/Contracts/Processing/Optimization/BookHeightModeDisplayNames.Designer.cs
@@ -22,7 +22,7 @@ namespace HomagConnect.MaterialManager.Contracts.Processing.Optimization {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class BookHeightModeDisplayNames {
+    public class BookHeightModeDisplayNames {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace HomagConnect.MaterialManager.Contracts.Processing.Optimization {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HomagConnect.MaterialManager.Contracts.Processing.Optimization.BookHeightModeDisp" +
