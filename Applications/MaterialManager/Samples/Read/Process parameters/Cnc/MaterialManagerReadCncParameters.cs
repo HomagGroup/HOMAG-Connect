434f074ec4841a55ce247bf03da0afa581fc096c
Liehner, Adrian <Adrian.Liehner@homag.com>
2025-07-03 10:18:21 +0200

    feat: add samples and tests

---
+++ Applications/MaterialManager/Samples/Read/Process parameters/Cnc/MaterialManagerReadCncParameters.cs
@@ -0,0 +1,43 @@
+﻿using HomagConnect.MaterialManager.Contracts.Processing.Interfaces;
+using HomagConnect.MaterialManager.Samples.Helper;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HomagConnect.MaterialManager.Samples.Read.Process_parameters
+{
+    /// <summary>
+    /// Sample methods to read CNC process parameter data using the materialManager CNC client.
+    /// </summary>
+    public static class MaterialManagerReadCncParameters
+    {
+        /// <summary>
+        /// Gets and traces all milling parameter parameter groups.
+        /// </summary>
+        public static async Task GetMillingParameterGroups(IMaterialManagerClientProcessingCnc cncClient)
+        {
+            var parameterGroups = await cncClient.GetMillingParameterGroups();
+            Assert.IsNotNull(parameterGroups);
+            parameterGroups.Trace();
+        }
+
+        /// <summary>
+        /// Gets and traces all milling parameter material groups.
+        /// </summary>
+        public static async Task GetMillingParameterMaterialGroups(IMaterialManagerClientProcessingCnc cncClient)
+        {
+            var materialGroups = await cncClient.GetMillingParameterMaterialGroups();
+            Assert.IsNotNull(materialGroups);
+            materialGroups.Trace();
+        }
+
+        /// <summary>
+        /// Gets and traces all milling parameter tool groups.
+        /// </summary>
+        public static async Task GetMillingParameterToolGroups(IMaterialManagerClientProcessingCnc cncClient)
+        {
+            var toolGroups = await cncClient.GetMillingParameterToolGroups();
+            Assert.IsNotNull(toolGroups);
+            toolGroups.Trace();
+        }
+    }
+}
---
+++ Applications/MaterialManager/Tests/Read/Process parameters/Cnc/MaterialManagerReadCncParameter.cs
@@ -0,0 +1,39 @@
+﻿using HomagConnect.MaterialManager.Samples.Read.Process_parameters;
+
+namespace HomagConnect.MaterialManager.Tests.Read.Process_parameters.Cnc
+{
+    /// <summary />
+    [TestClass]
+    [TestCategory("MaterialManager")]
+    [TestCategory("MaterialManager.Cnc.Read.Parameters")]
+    public class MaterialManagerReadCncParameter : MaterialManagerTestBase
+    {
+#pragma warning disable S2699 // Tests should include assertions
+
+        /// <summary />
+        [TestMethod]
+        public async Task GetMillingParameterToolGroups_GetResult_NoException()
+        {
+            var materialManager = GetMaterialManagerClient();
+            await MaterialManagerReadCncParameters.GetMillingParameterToolGroups(materialManager.Processing.Cnc);
+        }
+
+        /// <summary />
+        [TestMethod]
+        public async Task GetMillingParameterMaterialGroups_GetResult_NoException()
+        {
+            var materialManager = GetMaterialManagerClient();
+            await MaterialManagerReadCncParameters.GetMillingParameterMaterialGroups(materialManager.Processing.Cnc);
+        }
+
+        /// <summary />
+        [TestMethod]
+        public async Task GetMillingParameterGroups_GetResult_NoException()
+        {
+            var materialManager = GetMaterialManagerClient();
+            await MaterialManagerReadCncParameters.GetMillingParameterGroups(materialManager.Processing.Cnc);
+        }
+
+#pragma warning restore S2699 // Tests should include assertions
+    }
+}
