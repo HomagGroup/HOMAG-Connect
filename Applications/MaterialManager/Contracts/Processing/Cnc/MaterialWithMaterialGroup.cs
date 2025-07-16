010565c50af7f6462ef0fef975fe5b9ac67a9ed8
Liehner, Adrian <Adrian.Liehner@homag.com>
2025-07-16 08:02:00 +0200

    feat: add object for material object with material group to hg connect

---
+++ Applications/MaterialManager/Contracts/Processing/Cnc/MaterialWithMaterialGroup.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using Newtonsoft.Json;
+
+namespace HomagConnect.MaterialManager.Contracts.Processing.Cnc
+{
+    /// <summary>
+    /// Material with MaterialGroup used for Cnc process parameters.
+    /// </summary>
+    public class MaterialWithMaterialGroup : Material.Boards.Material
+    {
+        /// <summary>
+        /// The name of the MaterialGroup, unique per subscription
+        /// </summary>
+        [Key]
+        [JsonProperty(Order = 90)]
+        [Required]
+        public string MaterialGroupName { get; set; }
+    }
+}
