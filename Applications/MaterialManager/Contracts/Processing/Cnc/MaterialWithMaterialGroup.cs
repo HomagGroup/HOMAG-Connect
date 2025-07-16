4c4f1367f5172bc59004c68646f77dccb5fd1f1a
Liehner, Adrian <Adrian.Liehner@homag.com>
2025-07-16 14:13:47 +0200

    Update MaterialWithMaterialGroup.cs

--- Applications/MaterialManager/Contracts/Processing/Cnc/MaterialWithMaterialGroup.cs
+++ Applications/MaterialManager/Contracts/Processing/Cnc/MaterialWithMaterialGroup.cs
@@ -5,16 +5,15 @@ using Newtonsoft.Json;
 namespace HomagConnect.MaterialManager.Contracts.Processing.Cnc
 {
     /// <summary>
-    /// Material with MaterialGroup used for Cnc process parameters.
+    /// Material with MaterialGroup.
     /// </summary>
-    public class MaterialWithMaterialGroup : Material.Boards.Material
+    public class MaterialWithGroup : Material.Boards.Material
     {
         /// <summary>
         /// The name of the MaterialGroup, unique per subscription
         /// </summary>
-        [Key]
         [JsonProperty(Order = 90)]
         [Required]
-        public string MaterialGroupName { get; set; }
+        public string GroupName { get; set; }
     }
 }
