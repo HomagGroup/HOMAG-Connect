0cfc37b669a7b375384e44d0db3c5350009a02d2
Claudiu Toderas <claudiu.toderas@accesa.eu>
2025-07-18 10:49:33 +0300

    Add Models for Create Lot

---
+++ Applications/ProductionManager/Contracts/Lots/CreateLotRequest.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace HomagConnect.ProductionManager.Contracts.Lots
+{
+    /// <summary>
+    /// Object Model used for creating a lot
+    /// </summary>
+    public class CreateLotRequest
+    {
+        /// <summary>
+        /// Gets or sets the name of the lot.
+        /// </summary>
+        [JsonProperty(Order = 1)]
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the planned production start date of the lot.
+        /// </summary>
+        [JsonProperty(Order = 2)]
+        public DateTimeOffset? StartDatePlanned { get; set; }
+
+        /// <summary>
+        /// Gets or sets the planned production completion date of the lot.
+        /// </summary>
+        [JsonProperty(Order = 3)]
+        public DateTimeOffset? CompletionDatePlanned { get; set; }
+
+        /// <summary>
+        /// Gets or sets the part ids which will be added in the lot.
+        /// </summary>
+        [JsonProperty(Order = 4)]
+        public IEnumerable<Guid> PartIds { get; set; } = [];
+    }
+}
---
+++ Applications/ProductionManager/Contracts/Lots/CreateLotResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace HomagConnect.ProductionManager.Contracts.Lots
+{
+    /// <summary>
+    /// Lot creation operation response
+    /// </summary>
+    public class CreateLotResponse
+    {
+        /// <summary>
+        /// Gets or sets the correlation id for the import job
+        /// </summary>
+        [JsonProperty(Order = 1)]
+        public Guid LotId { get; set; }
+    }
+}
