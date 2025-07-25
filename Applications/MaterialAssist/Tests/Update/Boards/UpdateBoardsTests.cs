9c72a4e5b70db4c264dbd8077be0adc5e5b5a9bc
nic-1111 <nicole.helfenstein@homag.com>
2025-07-25 11:56:02 +0200

    tests for update

--- Applications/MaterialAssist/Tests/Update/Boards/UpdateBoardsTests.cs
+++ Applications/MaterialAssist/Tests/Update/Boards/UpdateBoardsTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using HomagConnect.MaterialAssist.Samples.Create.Boards;
+using HomagConnect.MaterialAssist.Samples.Delete.Boards;
+using HomagConnect.MaterialAssist.Samples.Get.Boards;
+using HomagConnect.MaterialAssist.Samples.Update.Boards;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,56 @@ using System.Threading.Tasks;
 
 namespace HomagConnect.MaterialAssist.Tests.Update.Boards
 {
-    public class UpdateBoardsTests
+    public class UpdateBoardsTests : MaterialAssistTestBase
     {
+
+        [ClassInitialize]
+        public async Task Initialize()
+        {
+            var MaterialAssistClient = GetMaterialAssistClient().Boards;
+            await CreateBoardEntitySample.Boards_CreateBoardEntity(MaterialAssistClient, "42", "50", "23");
+        }
+
+        [TestMethod]
+        public async Task BoardsUpdateBoardEntity()
+        {
+            var MaterialAssistClient = GetMaterialAssistClient().Boards;
+            await UpdateBoardEntitiesSamples.Boards_UpdateBoardEntity(MaterialAssistClient);
+        }
+
+        [TestMethod]
+        public async Task BoardsStoreBoardEntity()
+        {
+            var MaterialAssistClient = GetMaterialAssistClient().Boards;
+            await UpdateBoardEntitiesSamples.Boards_StoreBoardEntity(MaterialAssistClient);
+        }
+
+        [TestMethod]
+        public async Task BoardsRemoveAllBoardEntitiesFromWorkplace()
+        {
+            var MaterialAssistClient = GetMaterialAssistClient().Boards;
+            await UpdateBoardEntitiesSamples.Boards_RemoveAllBoardEntitiesFromWorkplace(MaterialAssistClient);
+        }
+
+        [TestMethod]
+        public async Task BoardsRemoveSubsetBoardEntitiesFromWorkplace()
+        {
+            var MaterialAssistClient = GetMaterialAssistClient().Boards;
+            await UpdateBoardEntitiesSamples.Boards_RemoveSubsetBoardEntitiesFromWorkplace(MaterialAssistClient);
+        }
+
+        [TestMethod]
+        public async Task BoardsRemoveSingleBoardEntitiesFromWorkplace()
+        {
+            var MaterialAssistClient = GetMaterialAssistClient().Boards;
+            await UpdateBoardEntitiesSamples.Boards_RemoveSingleBoardEntitiesFromWorkplace(MaterialAssistClient);
+        }
+
+        [ClassCleanup]
+        public async Task Cleanup()
+        {
+            var MaterialAssistClient = GetMaterialAssistClient().Boards;
+            await DeleteBoardEntitiesSamples.Boards_DeleteBoardEntities(MaterialAssistClient, ["42", "50", "23"]);
+        }
     }
 }
--- Applications/MaterialAssist/Tests/Update/Edgebands/UpdateEdgebandsTests.cs
+++ Applications/MaterialAssist/Tests/Update/Edgebands/UpdateEdgebandsTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using HomagConnect.MaterialAssist.Samples.Create.Edgebands;
+using HomagConnect.MaterialAssist.Samples.Delete.Edgebands;
+using HomagConnect.MaterialAssist.Samples.Get.Edgebands;
+using HomagConnect.MaterialAssist.Samples.Update.Edgebands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,60 @@ using System.Threading.Tasks;
 
 namespace HomagConnect.MaterialAssist.Tests.Update.Edgebands
 {
-    internal class UpdateEdgebandsTests
+    public class UpdateEdgebandsTests : MaterialAssistTestBase
     {
+
+        [ClassInitialize]
+        public async Task Initialize()
+        {
+            var MaterialAssistClient = GetMaterialAssistClient().Edgebands;
+            await CreateEdgebandEntitiesSamples.Edgebands_CreateEdgebandEntity(MaterialAssistClient, "42");
+            await CreateEdgebandEntitiesSamples.Edgebands_CreateEdgebandEntity(MaterialAssistClient, "50");
+            await CreateEdgebandEntitiesSamples.Edgebands_CreateEdgebandEntity(MaterialAssistClient, "23");
+        }
+
+        [TestMethod]
+        public async Task EdgebandsUpdateEdgebandEntities()
+        {
+            var MaterialAssistClient = GetMaterialAssistClient().Edgebands;
+            await UpdateEdgebandEntitiesSamples.Edgebands_UpdateEdgebandEntities(MaterialAssistClient);
+        }
+
+        [TestMethod]
+        public async Task EdgebandsStoreEdgebandEntities()
+        {
+            var MaterialAssistClient = GetMaterialAssistClient().Edgebands;
+            await UpdateEdgebandEntitiesSamples.Edgebands_StoreEdgebandEntities(MaterialAssistClient);
+        }
+
+        [TestMethod]
+        public async Task EdgebandsRemoveAllEdgebandEntitiesFromWorkplace()
+        {
+            var MaterialAssistClient = GetMaterialAssistClient().Edgebands;
+            await UpdateEdgebandEntitiesSamples.Edgebands_RemoveAllEdgebandEntitiesFromWorkplace(MaterialAssistClient);
+        }
+
+        [TestMethod]
+        public async Task EdgebandsRemoveSubsetEdgebandEntitiesFromWorkplace()
+        {
+            var MaterialAssistClient = GetMaterialAssistClient().Edgebands;
+            await UpdateEdgebandEntitiesSamples.Edgebands_RemoveSubsetEdgebandEntitiesFromWorkplace(MaterialAssistClient);
+        }
+
+        [TestMethod]
+        public async Task EdgebandsRemoveSingleEdgebandEntitiesFromWorkplace()
+        {
+            var MaterialAssistClient = GetMaterialAssistClient().Edgebands;
+            await UpdateEdgebandEntitiesSamples.Edgebands_RemoveSingleEdgebandEntitiesFromWorkplace(MaterialAssistClient);
+        }
+
+        [ClassCleanup]
+        public async Task Cleanup()
+        {
+            var MaterialAssistClient = GetMaterialAssistClient().Edgebands;
+            await DeleteEdgebandEntitiesSamples.Edgebands_DeleteEdgebandEntity(MaterialAssistClient, "42");
+            await DeleteEdgebandEntitiesSamples.Edgebands_DeleteEdgebandEntity(MaterialAssistClient, "50");
+            await DeleteEdgebandEntitiesSamples.Edgebands_DeleteEdgebandEntity(MaterialAssistClient, "23");
+        }
     }
 }
