8143b4f4ba68f09653ecc8a577a95513944a0191
Boris Wehrle (HOMAG) <141398900+HOMAG-Boris-Wehrle@users.noreply.github.com>
2025-08-11 15:37:56 +0200

    Production protocol dtos added. (#399)
    * Production protocol dtos added.
    
    * ProcessedBoard introduced

---
+++ Base/HomagConnect.Base.Contracts/Enumerations/MachineType.cs
@@ -0,0 +1,22 @@
+﻿namespace HomagConnect.Base.Contracts.Enumerations;
+
+/// <summary>
+/// Represents the type of machine used in the production process.
+/// </summary>
+public enum MachineType
+{
+    /// <summary>
+    /// Unknown machine type (e.g. not specified or not recognized)
+    /// </summary>
+    Unknown,
+
+    /// <summary>
+    /// Cutting machine (e.g. SAWTEQ)
+    /// </summary>
+    Cutting,
+
+    /// <summary>
+    /// CNC machine (e.g. CENTATEQ)
+    /// </summary>
+    Cnc
+}
