851fe39641ac442e54641636aefa3b6a3cd6803f
Adrian Liehner <145965882+aldfhg@users.noreply.github.com>
2025-08-19 16:14:02 +0200

    feat: add documentation (#409)
    * feat: add documentation
    
    * feat: fix and add samples

--- Applications/MaterialManager/Samples/Create/Boards/Readme.md
+++ Applications/MaterialManager/Samples/Create/Boards/Readme.md
@@ -26,3 +26,37 @@ var newBoardType = await client.CreateBoardType(boardTypeRequest);
 
 Console.WriteLine($"Created Board Type: {newBoardType.Code}");
 ```
+
+It is also possible to attatch additional data, like a image, to a board type at creation.
+
+<strong>Example:</strong>
+
+```csharp
+var client = new MaterialManagerClientMaterialBoards(subscriptionId, authorizationKey);
+
+var boardTypeRequest = new MaterialManagerRequestBoardType
+{
+    //The material code is the identifier of the material type
+    MaterialCode = "HPL_F274_9_12.0",
+    //The board code is the identifier of the board type
+    BoardCode = "HPL_F274_9_12.0_4100.0_650.0",
+    Length = 2070.0,
+    Width = 2800.0,
+    Thickness = 19.0,
+    Type = BoardTypeType.Board,
+    MaterialCategory = BoardMaterialCategory.Undefined,
+    CoatingCategory CoatingCategory = CoatingCategory.Undefined,
+    Grain = Grain.None,
+};
+
+var testFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "Red.png");
+var fileReferences = new FileReference[]
+{
+    new FileReference("BoardPicture", testFilePath)
+};
+
+var newBoardType = await client.CreateBoardType(boardTypeRequest, fileReferences);
+
+Console.WriteLine($"Created Board Type: {newBoardType.Code}");
+```
+
--- Applications/MaterialManager/Tests/Create/Edgebands/CreateEdgebandTypeTests.cs
+++ Applications/MaterialManager/Tests/Create/Edgebands/CreateEdgebandTypeTests.cs
@@ -1,9 +1,12 @@
-﻿using FluentAssertions;
+﻿using System.ComponentModel.DataAnnotations;
+
+using FluentAssertions;
+
+using HomagConnect.Base.Contracts;
 using HomagConnect.MaterialManager.Client;
+using HomagConnect.MaterialManager.Contracts.Material.Edgebands.Enumerations;
 using HomagConnect.MaterialManager.Contracts.Request;
-using HomagConnect.MaterialManager.Samples.Create.Boards;
 using HomagConnect.MaterialManager.Samples.Create.Edgebands;
-using System.ComponentModel.DataAnnotations;
 
 namespace HomagConnect.MaterialManager.Tests.Create.Edgebands;
 
@@ -12,35 +15,6 @@ namespace HomagConnect.MaterialManager.Tests.Create.Edgebands;
 public class CreateEdgebandTypeTests : MaterialManagerTestBase
 {
     /// <summary />
-    [TestMethod]
-    [DataRow(null, 20, 1.2, 150)] // EdgebandCode not set
-    [DataRow("EdgebandCode", null, 1.2, 150)] // Height not set
-    [DataRow("EdgebandCode", 20, null, 150)] // Thickness not set
-    [DataRow("EdgebandCode", 20, 1.2, null)] // Length not set
-    public async Task EdgebandTypeCreation_RequiredPropertiesMissing_ThrowsException(string edgebandCode, double height, double thickness, double length)
-    {
-        var client = new HttpClient();
-        var edgebandsClient = new MaterialManagerClientMaterialEdgebands(client);
-
-        var requestEdgebandType = CreateEdgebandTypeRequest(edgebandCode, height, thickness, length);
-        
-        var act = async () => await edgebandsClient.CreateEdgebandType(requestEdgebandType);
-
-        await act.Should().ThrowAsync<ValidationException>();
-    }
-
-    private static MaterialManagerRequestEdgebandType CreateEdgebandTypeRequest(string edgebandCode, double height, double thickness, double length)
-    {
-        var edgebandType = new MaterialManagerRequestEdgebandType();
-        edgebandType.EdgebandCode = edgebandCode;
-        edgebandType.Height = height;
-        edgebandType.Thickness = thickness;
-        edgebandType.DefaultLength = length;
-        return edgebandType;
-    }
-
-    /// <summary /> 
-
     [TestMethod]
     public async Task BoardsCreateBoardType()
     {
@@ -54,6 +28,44 @@ public class CreateEdgebandTypeTests : MaterialManagerTestBase
     {
         var materialManagerClient = GetMaterialManagerClient();
         await materialManagerClient.Material.Edgebands.DeleteEdgebandType("EB_White_1mm");
+        await materialManagerClient.Material.Edgebands.DeleteEdgebandType("EB_White_1mm_AdditionalData");
+    }
+
+    /// <summary />
+    [TestMethod]
+    [DataRow(null, 20, 1.2, 150)] // EdgebandCode not set
+    [DataRow("EdgebandCode", null, 1.2, 150)] // Height not set
+    [DataRow("EdgebandCode", 20, null, 150)] // Thickness not set
+    [DataRow("EdgebandCode", 20, 1.2, null)] // Length not set
+    public async Task EdgebandTypeCreation_RequiredPropertiesMissing_ThrowsException(string edgebandCode, double height, double thickness, double length)
+    {
+        var client = new HttpClient();
+        var edgebandsClient = new MaterialManagerClientMaterialEdgebands(client);
+
+        var requestEdgebandType = CreateEdgebandTypeRequest(edgebandCode, height, thickness, length);
+
+        var act = async () => await edgebandsClient.CreateEdgebandType(requestEdgebandType);
+
+        await act.Should().ThrowAsync<ValidationException>();
+    }
+
+    [TestMethod]
+    public async Task EdgebandTypeCreation_WithAdditionalData_Succeeds()
+    {
+        var materialManagerClient = GetMaterialManagerClient();
+        var edgebandCode = "EB_White_1mm_AdditionalData";
+
+        await CreateEdgebandTypeSamples.Edgebands_CreateEdgebandType_AdditionalData(
+            materialManagerClient.Material.Edgebands, edgebandCode);
+    }
+
+    private static MaterialManagerRequestEdgebandType CreateEdgebandTypeRequest(string edgebandCode, double height, double thickness, double length)
+    {
+        var edgebandType = new MaterialManagerRequestEdgebandType();
+        edgebandType.EdgebandCode = edgebandCode;
+        edgebandType.Height = height;
+        edgebandType.Thickness = thickness;
+        edgebandType.DefaultLength = length;
+        return edgebandType;
     }
 }
-
