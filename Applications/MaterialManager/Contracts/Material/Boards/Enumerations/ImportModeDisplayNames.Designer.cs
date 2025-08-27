161c684d635cdf221e310cd87ddb9e1cad839fa1
Peter <peter.lorincz@principal33.com>
2025-08-27 17:20:14 +0300

    make public

--- Applications/MaterialManager/Contracts/Material/Boards/Enumerations/ImportModeDisplayNames.Designer.cs
+++ Applications/MaterialManager/Contracts/Material/Boards/Enumerations/ImportModeDisplayNames.Designer.cs
@@ -22,7 +22,7 @@ namespace HomagConnect.MaterialManager.Contracts.Material.Boards.Enumerations {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class ImportModeDisplayNames {
+    public class ImportModeDisplayNames {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace HomagConnect.MaterialManager.Contracts.Material.Boards.Enumerations {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HomagConnect.MaterialManager.Contracts.Material.Boards.Enumerations.ImportModeDis" +
--- Applications/MaterialManager/Contracts/Material/Boards/Enumerations/InventoryTypeDisplayNames.Designer.cs
+++ Applications/MaterialManager/Contracts/Material/Boards/Enumerations/InventoryTypeDisplayNames.Designer.cs
@@ -22,7 +22,7 @@ namespace HomagConnect.MaterialManager.Contracts.Material.Boards.Enumerations {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class InventoryTypeDisplayNames {
+    public class InventoryTypeDisplayNames {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace HomagConnect.MaterialManager.Contracts.Material.Boards.Enumerations {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HomagConnect.MaterialManager.Contracts.Material.Boards.Enumerations.InventoryType" +
