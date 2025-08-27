06298e1d8f1c434f30aa9f9168ab669c659de9d7
peter-lorincz-duerr <peter.lorincz@cadrantechnologies.eu>
2025-08-27 16:14:48 +0300

    add missing properties and ImportInventoryRequest (#421)
    * add missing properties and ImportInventoryRequest
    
    * remove duplicate resource
    
    * add missing resx files
    
    ---------
    
    Co-authored-by: Peter <peter.lorincz@principal33.com>

---
+++ Applications/MaterialManager/Contracts/Material/Boards/Enumerations/ImportMode.cs
@@ -0,0 +1,27 @@
+﻿using HomagConnect.Base.Contracts.Attributes;
+using HomagConnect.Base.Contracts.Converter;
+
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomagConnect.MaterialManager.Contracts.Material.Boards.Enumerations
+{
+    /// <summary>
+    /// </summary>
+    [ResourceManager(typeof(ImportModeDisplayNames))]
+    [JsonConverter(typeof(TolerantEnumConverter))]
+    public enum ImportMode
+    {
+        /// <summary>
+        /// Use for initial import or re-sync.
+        /// </summary>
+        [Display(Description = "Full")]
+        Full,
+
+        /// <summary>
+        /// Upsert the data you send.
+        /// </summary>
+        [Display(Description = "Partial")]
+        Partial
+    }
+}
