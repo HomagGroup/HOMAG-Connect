abfecd2136e66b9aa8287a5529a54bc611ec9bd6
Boris Wehrle (HOMAG) <141398900+HOMAG-Boris-Wehrle@users.noreply.github.com>
2025-08-28 15:16:49 +0200

    Contracts for AppEvents (intelliDivide, productionManager) (#415)
    * intelliDivide events introduced
    
    * Id added
    
    * SolutionTransferredEvent_SerializeDeserialize_PropertiesEqual sample added
    
    * Events simplified
    
    * ProductionItemProcessedByMachineFileEvent_SerializeDeserialize_PropertiesEqual added
    
    * Tests aligned
    
    * Properties sorted
    
    * Events restructured

---
+++ Base/HomagConnect.Base.Contracts/Events/AppEvent.cs
@@ -0,0 +1,90 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using HomagConnect.Base.Contracts.Attributes;
+using HomagConnect.Base.Contracts.Interfaces;
+
+using Newtonsoft.Json;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+namespace HomagConnect.Base.Contracts.Events;
+
+// Note: This is preliminary code and is subject to change
+
+/// <summary>
+/// Represents a base class for application events.
+/// </summary>
+public class AppEvent : IAppEvent
+{
+    /// <summary>
+    /// Creates a new instance of the <see cref="AppEvent" /> class.
+    /// </summary>
+    public AppEvent()
+    {
+        Id = Guid.NewGuid();
+        Timestamp = DateTimeOffset.Now;
+
+        if (GetType() != typeof(AppEvent))
+        {
+            var appEventAttribute = this.GetType().GetCustomAttributes(typeof(AppEventAttribute), false).OfType<AppEventAttribute>().FirstOrDefault();
+
+            if (appEventAttribute == null)
+            {
+                throw new ArgumentNullException("AppEventAttribute missing on type " + GetType());
+            }
+
+            Key = appEventAttribute.Key;
+        }
+    }
+
+    /// <summary>
+    /// Gets a value indicating whether the current event instance is valid.
+    /// </summary>
+    [JsonIgnore]
+    public bool IsValid
+    {
+        get
+        {
+            var context = new ValidationContext(this);
+            var results = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(this, context, results, true);
+
+            return isValid;
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the subscription ID of the event.
+    /// </summary>
+    [JsonProperty(Order = 2)]
+    [Required]
+    public Guid SubscriptionId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the additional properties configured in the application.
+    /// </summary>
+    [JsonProperty(Order = 90)]
+    [JsonExtensionData]
+    public IDictionary<string, object> CustomProperties { get; set; }
+
+    /// <summary>
+    /// Gets or sets the unique identifier of the event.
+    /// </summary>
+    [JsonProperty(Order = 0)]
+    [Required]
+    public Guid Id { get; private set; }
+
+    /// <summary>
+    /// Get or sets the key of the event.
+    /// </summary>
+    [JsonProperty(Order = 3)]
+    [Required]
+    public string Key { get; private set; }
+
+    /// <summary>
+    /// Gets or sets the time offset of the event.
+    /// </summary>
+    [JsonProperty(Order = 1)]
+    [Required]
+    public DateTimeOffset Timestamp { get; set; }
+}
