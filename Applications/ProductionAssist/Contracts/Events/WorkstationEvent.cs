ce8c917f81ff96e3a270e262408c8508d410c06c
Boris Wehrle (HOMAG) <141398900+HOMAG-Boris-Wehrle@users.noreply.github.com>
2025-09-25 11:00:13 +0200

    Events restructured

---
+++ Applications/ProductionAssist/Contracts/Events/WorkstationEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using HomagConnect.Base.Contracts.Events;
+
+using Newtonsoft.Json;
+
+namespace HomagConnect.ProductionAssist.Contracts.Events;
+
+/// <summary>
+/// Base class for events that occur on a workstation.
+/// </summary>
+public abstract class WorkstationEvent : AppEvent
+{
+    /// <summary>
+    /// Gets or sets the Identifier of the Workstation where the event occurred.
+    /// </summary>
+    [Required]
+    [JsonProperty(Order = 10)]
+    public Guid WorkstationId { get; set; }
+}
