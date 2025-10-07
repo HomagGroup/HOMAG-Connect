b06660c393a26619806253135928460bdabe7076
nic-1111 <nicole.helfenstein@homag.com>
2025-10-07 09:27:46 +0200

    boards

--- Applications/MaterialAssist/Tests/Integration/Boards/CreateUpdateAllocateBoards.cs
+++ Applications/MaterialAssist/Tests/Integration/Boards/CreateUpdateAllocateBoards.cs
@@ -18,55 +18,24 @@ namespace HomagConnect.MaterialAssist.Tests.Integration
         public  async Task ClassInit()
         {
             await EnsureBoardTypeExist("HPL_F274_9_19.0");
+            await EnsureBoardTypeExist("P2_F204_75_38.0", 4100, 600);
+            await EnsureBoardTypeExist("HPL_Natural_4.0", 2790, 2060);
         }
 
         [TestMethod]
         public async Task Boards_CreateUpdateAllocate()
         {
-            // create board types
-            var materialManagerClient = GetMaterialManagerClient().Material.Boards;
-            var boardTypeRequest = new MaterialManagerRequestBoardType
-            {
-                MaterialCode = "P2_F204_75_38.0",
-                BoardCode = "P2_F204_75_38.0_4100.0_600.0",
-                Length = 4100.0,
-                Width = 600.0,
-                Thickness = 38.0,
-                Type = BoardTypeType.Board,
-                MaterialCategory = BoardMaterialCategory.Chipboard,
-                CoatingCategory = CoatingCategory.Undefined,
-                Grain = Grain.None,
-            };
-            await materialManagerClient.CreateBoardType(boardTypeRequest);
-            var boardTypeResponse = await materialManagerClient.GetBoardTypeByBoardCode(boardTypeRequest.BoardCode);
-            Assert.AreEqual(4100.0, boardTypeResponse.Length);
-            Assert.AreEqual(600.0, boardTypeResponse.Width);
-            Assert.AreEqual(38.0, boardTypeResponse.Thickness);
-
-            var boardTypeRequest2 = new MaterialManagerRequestBoardType
-            {
-                MaterialCode = "HPL_Natural_4.0",
-                BoardCode = "HPL_Natural_4.0_2790.0_2060.0",
-                Length = 2790.0,
-                Width = 2060.0,
-                Thickness = 4.0,
-                Type = BoardTypeType.Board,
-                MaterialCategory = BoardMaterialCategory.Undefined,
-                CoatingCategory = CoatingCategory.Undefined,
-                Grain = Grain.None,
-            };
-            await materialManagerClient.CreateBoardType(boardTypeRequest2);
-            var boardTypeResponse2 = await materialManagerClient.GetBoardTypeByBoardCode(boardTypeRequest2.BoardCode);
-            Assert.AreEqual(2790.0, boardTypeResponse2.Length);
-            Assert.AreEqual(2060.0, boardTypeResponse2.Width);
-            Assert.AreEqual(4.0, boardTypeResponse2.Thickness);
+            // constant data
+            var board1 = new { id = "93", boardCode = "HPL_F274_9_19.0_2800_2070" };
+            var board2 = new { id = "91", boardCode = "P2_F204_75_38.0_4100_600" };
+            var board3 = new { id = "92", boardCode = "HPL_Natural_4.0_2790_2060" };
 
             // create board entities
             var materialAssistClient = GetMaterialAssistClient().Boards;
             var boardEntityRequest = new MaterialAssistRequestBoardEntity()
             {
-                Id = "93",
-                BoardCode = "HPL_F274_9_19.0_2800_2070",
+                Id = board1.id,
+                BoardCode = board1.boardCode,
                 ManagementType = ManagementType.Single,
                 Quantity = 1
             };
@@ -74,8 +43,8 @@ namespace HomagConnect.MaterialAssist.Tests.Integration
 
             var boardEntityRequest2 = new MaterialAssistRequestBoardEntity()
             {
-                Id = "91",
-                BoardCode = "P2_F204_75_38.0_4100.0_600.0",
+                Id = board2.id,
+                BoardCode = board2.boardCode,
                 ManagementType = ManagementType.Stack,
                 Quantity = 10
             };
@@ -83,112 +52,79 @@ namespace HomagConnect.MaterialAssist.Tests.Integration
 
             var boardEntityRequest3 = new MaterialAssistRequestBoardEntity()
             {
-                Id = "92",
-                BoardCode = "HPL_Natural_4.0_2790.0_2060.0",
+                Id = board3.id,
+                BoardCode = board3.boardCode,
                 ManagementType = ManagementType.GoodsInStock,
                 Quantity = 2
             };
             await materialAssistClient.CreateBoardEntity(boardEntityRequest3);
 
-            /* store function not on prd yet
-            // store board entities
-            var storeBoardEntity = new MaterialAssistStoreBoardEntity()
-            {
-                Id = "93",
-                Length = 2800,
-                Width = 2070,
-                StorageLocation = { Name = "001" },
-            };
-            await materialAssistClient.StoreBoardEntity(storeBoardEntity);
-            var boardEntity = await materialAssistClient.GetBoardEntityById("93");
-            Assert.AreEqual("001", boardEntity.Location.Name);
-
-            var storeBoardEntity2 = new MaterialAssistStoreBoardEntity()
-            {
-                Id = "91",
-                Length = 4100,
-                Width = 600,
-                StorageLocation = { Name = "002" },
-            };
-            await materialAssistClient.StoreBoardEntity(storeBoardEntity2);
-            var boardEntity2 = await materialAssistClient.GetBoardEntityById("91");
-            Assert.AreEqual("002", boardEntity2.Location.Name);
-
-            var storeBoardEntity3 = new MaterialAssistStoreBoardEntity()
-            {
-                Id = "92",
-                Length = 2790,
-                Width = 2060,
-                StorageLocation = { Name = "003" },
-            };
-            await materialAssistClient.StoreBoardEntity(storeBoardEntity3);
-            var boardEntity3 = await materialAssistClient.GetBoardEntityById("92");
-            Assert.AreEqual("003", boardEntity3.Location.Name);
-            */
-
             // update board types
-            var boardType1 = await materialManagerClient.GetBoardTypeByBoardCodeIncludingDetails("HPL_F274_9_19.0_2800_2070");
+            var materialManagerClient = GetMaterialManagerClient().Material.Boards;
+            var boardType1 = await materialManagerClient.GetBoardTypeByBoardCodeIncludingDetails(board1.boardCode);
 
             var boardTypeUpdate = new MaterialManagerUpdateBoardType
             {
                 BoardCode = "HPL_F274_9_12.0_2800_2070",
                 Thickness = 12.0,
             };
-            await materialManagerClient.UpdateBoardType("HPL_F274_9_19.0_2800_2070", boardTypeUpdate);
-            var updatedBoardType1 = await materialManagerClient.GetBoardTypeByBoardCodeIncludingDetails("HPL_F274_9_12.0_2800_2070");
+            await materialManagerClient.UpdateBoardType(board1.boardCode, boardTypeUpdate);
+            var updatedBoard1 = new { id = "93", boardCode = "HPL_F274_9_12.0_2800_2070" };
+            var updatedBoardType1 = await materialManagerClient.GetBoardTypeByBoardCodeIncludingDetails(updatedBoard1.boardCode);
             Assert.AreEqual(12.0, updatedBoardType1.Thickness);
 
-            var boardType2 = await materialManagerClient.GetBoardTypeByBoardCodeIncludingDetails("P2_F204_75_38.0_4100.0_600.0");
+            var boardType2 = await materialManagerClient.GetBoardTypeByBoardCodeIncludingDetails(board2.boardCode);
             var boardTypeUpdate2 = new MaterialManagerUpdateBoardType
             {
-                BoardCode = "P2_F204_75_38.0_4200.0_610.0",
+                BoardCode = "P2_F204_75_38.0_4200_610",
                 Length = 4200.0,
                 Width = 610.0,
             };
-            await materialManagerClient.UpdateBoardType("P2_F204_75_38.0_4100.0_600.0", boardTypeUpdate2);
-            var updatedBoardType2 = await materialManagerClient.GetBoardTypeByBoardCodeIncludingDetails("P2_F204_75_38.0_4200.0_610.0");
+            await materialManagerClient.UpdateBoardType(board2.boardCode, boardTypeUpdate2);
+            var updatedBoard2 = new { id = "91", boardCode = "P2_F204_75_38.0_4200_610" };
+            var updatedBoardType2 = await materialManagerClient.GetBoardTypeByBoardCodeIncludingDetails(updatedBoard2.boardCode);
             Assert.AreEqual(4200.0, updatedBoardType2.Length);
             Assert.AreEqual(610.0, updatedBoardType2.Width);
 
-            var boardType3 = await materialManagerClient.GetBoardTypeByBoardCodeIncludingDetails("HPL_Natural_4.0_2790.0_2060.0");
+            var boardType3 = await materialManagerClient.GetBoardTypeByBoardCodeIncludingDetails(board3.boardCode);
             var boardTypeUpdate3 = new MaterialManagerUpdateBoardType
             {
                 MaterialCategory = BoardMaterialCategory.CompactPanels_HPL,
             };
-            await materialManagerClient.UpdateBoardType("HPL_Natural_4.0_2790.0_2060.0", boardTypeUpdate3);
-            var updatedBoardType3 = await materialManagerClient.GetBoardTypeByBoardCodeIncludingDetails("HPL_Natural_4.0_2790.0_2060.0");
+            await materialManagerClient.UpdateBoardType(board3.boardCode, boardTypeUpdate3);
+            var updatedBoardType3 = await materialManagerClient.GetBoardTypeByBoardCodeIncludingDetails(board3.boardCode);
             Assert.AreEqual(BoardMaterialCategory.CompactPanels_HPL, updatedBoardType3.MaterialCategory);
 
             // create board type allocations
             var boardTypeAllocationRequest = new BoardTypeAllocationRequest
             {
-                BoardTypeCode = "HPL_F274_9_12.0_2800_2070",
+                BoardTypeCode = updatedBoard1.boardCode,
                 CreatedBy = "DeploymentTest",
                 Name = "DeploymentTestAllocation",
                 Quantity = 1,
-                Source = "test",
+                Source = "HOMAG Connect",
                 Workstation = "001"
             };
             await materialManagerClient.CreateBoardTypeAllocation(boardTypeAllocationRequest);
 
             var boardTypeAllocationRequest2 = new BoardTypeAllocationRequest
             {
-                BoardTypeCode = "P2_F204_75_38.0_4200.0_610.0",
+                BoardTypeCode = updatedBoard2.boardCode,
                 CreatedBy = "DeploymentTest",
                 Name = "2DeploymentTestAllocation2",
                 Quantity = 1,
-                Source = "test",
+                Source = "HOMAG Connect",
                 Workstation = "001"
             };
             await materialManagerClient.CreateBoardTypeAllocation(boardTypeAllocationRequest2);
 
             var boardTypeAllocationRequest3 = new BoardTypeAllocationRequest
             {
-                BoardTypeCode = "HPL_Natural_4.0_2790.0_2060.0",
+                BoardTypeCode = board3.boardCode,
                 CreatedBy = "DeploymentTest",
                 Name = "3DeploymentTestAllocation3",
                 Quantity = 2,
-                Source = "test",
+                Source = "HOMAG Connect",
                 Workstation = "001"
             };
             await materialManagerClient.CreateBoardTypeAllocation(boardTypeAllocationRequest3);
@@ -198,15 +134,72 @@ namespace HomagConnect.MaterialAssist.Tests.Integration
             Assert.IsTrue(allAllocationNames.Any(a => a.Name == "DeploymentTestAllocation"));
             Assert.IsTrue(allAllocationNames.Any(a => a.Name == "2DeploymentTestAllocation2"));
             Assert.IsTrue(allAllocationNames.Any(a => a.Name == "3DeploymentTestAllocation3"));
+
+            // needed for storing
+            // Get the first workstation
+            var workstations = await materialAssistClient.GetWorkstations().ConfigureAwait(false);
+            var firstWorkstation = workstations.FirstOrDefault();
+            if (firstWorkstation == null)
+            {
+                Assert.Inconclusive("No workstations found.");
+                return;
+            }
+            // Get the first storage location for the workstation
+            var storageLocations = await materialAssistClient.GetStorageLocations(firstWorkstation.Id.ToString()).ConfigureAwait(false);
+            var firstStorageLocation = storageLocations.FirstOrDefault();
+            if (firstStorageLocation == null)
+            {
+                Assert.Inconclusive("No storage locations found for the workstation.");
+                return;
+            }
+
+            // store board entities
+            var storeBoardEntity = new MaterialAssistStoreBoardEntity()
+            {
+                Id = "93",
+                Length = 2800,
+                Width = 2070,
+                StorageLocation = firstStorageLocation,
+            };
+            await materialAssistClient.StoreBoardEntity(storeBoardEntity);
+            var boardEntity = await materialAssistClient.GetBoardEntityById("93");
+            Assert.AreEqual(firstStorageLocation.Name, boardEntity.Location.Name);
+
+            var storeBoardEntity2 = new MaterialAssistStoreBoardEntity()
+            {
+                Id = "91",
+                Length = 4100,
+                Width = 600,
+                StorageLocation = firstStorageLocation,
+            };
+            await materialAssistClient.StoreBoardEntity(storeBoardEntity2);
+            var boardEntity2 = await materialAssistClient.GetBoardEntityById("91");
+            Assert.AreEqual(firstStorageLocation.Name, boardEntity2.Location.Name);
+
+            var storeBoardEntity3 = new MaterialAssistStoreBoardEntity()
+            {
+                Id = "92",
+                Length = 2790,
+                Width = 2060,
+                StorageLocation = firstStorageLocation,
+            };
+            await materialAssistClient.StoreBoardEntity(storeBoardEntity3);
+            var boardEntity3 = await materialAssistClient.GetBoardEntityById("92");
+            Assert.AreEqual(firstStorageLocation.Name, boardEntity3.Location.Name);
+            
         }
-        
+
         [TestCleanup]
         public async Task Cleanup()
         {
+            var materialAssistClient = GetMaterialAssistClient().Boards;
+            await materialAssistClient.DeleteBoardEntity("93");
+            await materialAssistClient.DeleteBoardEntity("91");
+            await materialAssistClient.DeleteBoardEntity("92");
             var materialManagerClient = GetMaterialManagerClient().Material;
             await materialManagerClient.Boards.DeleteBoardType("HPL_F274_9_12.0_2800_2070");
-            await materialManagerClient.Boards.DeleteBoardType("P2_F204_75_38.0_4200.0_610.0");
-            await materialManagerClient.Boards.DeleteBoardType("HPL_Natural_4.0_2790.0_2060.0");
+            await materialManagerClient.Boards.DeleteBoardType("P2_F204_75_38.0_4200_610");
+            await materialManagerClient.Boards.DeleteBoardType("HPL_Natural_4.0_2790_2060");
         }
     }
 }
