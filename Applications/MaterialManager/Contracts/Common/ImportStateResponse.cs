c8ecbbb6bbc036a0d137e58a7d83d8a37c957624
Daniel Schaber <39241151+DaniSchaber@users.noreply.github.com>
2025-10-13 17:27:19 +0200

    feat: import state and response (#454)
    * feat: import state and response
    
    * improve update test. Thickness cannot be changed
    
    * add unknown

---
+++ Applications/MaterialManager/Contracts/Common/ImportStateResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HomagConnect.MaterialManager.Contracts.Common;
+
+/// <summary>
+/// Returned response when querying the state of an import process.
+/// </summary>
+public class ImportStateResponse
+{
+    string CorrelationId { get; set; } = string.Empty;
+    
+    ImportState ImportState { get; set; }
+    
+    DateTimeOffset? ImportSuccessTime { get; set; }
+}
