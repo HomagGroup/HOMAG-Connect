643b45b9363bf551d00adba5602108f5ebce5c92
Daniel <daniel.schaber@codeunity.de>
2025-10-13 14:18:15 +0200

    feat: import state and response

---
+++ Applications/MaterialManager/Contracts/Common/ImportState.cs
@@ -0,0 +1,33 @@
+﻿using HomagConnect.Base.Contracts.Converter;
+
+using Newtonsoft.Json;
+
+namespace HomagConnect.MaterialManager.Contracts.Common;
+
+/// <summary>
+/// Import state
+/// </summary>
+/// <summary>The state of an order import process</summary>
+[JsonConverter(typeof(TolerantEnumConverter))]
+public enum ImportState
+{
+    /// <summary>
+    /// Queued
+    /// </summary>
+    Queued,
+
+    /// <summary>
+    /// InProgress
+    /// </summary>
+    InProgress,
+
+    /// <summary>
+    /// Succeeded
+    /// </summary>
+    Succeeded,
+
+    /// <summary>
+    /// Error
+    /// </summary>
+    Error
+}
