54be303b8569a668d17cefff02aa5534a089b390
Liehner, Adrian <Adrian.Liehner@homag.com>
2025-10-24 10:03:36 +0200

    feat: add new sorting events

---
+++ Applications/ProductionAssist/Contracts/Events/Sorting/ProductionItemPickedFromShelfEvent.cs
@@ -0,0 +1,13 @@
+﻿#nullable enable
+
+using HomagConnect.Base.Contracts.Attributes;
+using HomagConnect.ProductionAssist.Contracts.Events.Feedback;
+
+namespace HomagConnect.ProductionAssist.Contracts.Events.Sorting
+{
+    /// <summary>
+    /// Event that occurs when a production item has been picked from a shelf.
+    /// </summary>
+    [AppEvent(nameof(ProductionAssist) + "." + nameof(Sorting) + "." + nameof(ProductionItemPickedFromShelfEvent))]
+    public class ProductionItemPickedFromShelfEvent : ProductionItemCompletedEvent;
+}
---
+++ Applications/ProductionAssist/Contracts/Events/Sorting/ProductionItemPlacedInShelfEvent.cs
@@ -0,0 +1,13 @@
+﻿#nullable enable
+
+using HomagConnect.Base.Contracts.Attributes;
+using HomagConnect.ProductionAssist.Contracts.Events.Feedback;
+
+namespace HomagConnect.ProductionAssist.Contracts.Events.Sorting
+{
+    /// <summary>
+    /// Event that occurs when a production item has been placed in a shelf.
+    /// </summary>
+    [AppEvent(nameof(ProductionAssist) + "." + nameof(Sorting) + "." + nameof(ProductionItemPlacedInShelfEvent))]
+    public class ProductionItemPlacedInShelfEvent : ProductionItemCompletedEvent;
+}
