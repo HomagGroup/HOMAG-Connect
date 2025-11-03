826091be555edbbda0eb16b6be0dacd3abe2ce97
Oana Sabadas <oana.sabadas@accesa.eu>
2025-11-03 14:49:12 +0200

    revert MachineType enum to hold only Cutting, Cnc, and Unknown

--- Base/HomagConnect.Base.Contracts/Enumerations/MachineType.cs
+++ Base/HomagConnect.Base.Contracts/Enumerations/MachineType.cs
@@ -21,17 +21,7 @@ public enum MachineType
     Cutting,
 
     /// <summary>
-    /// Cutting machine (e.g. CENTATEQ)
-    /// </summary>
-    Nesting,
-
-    /// <summary>
     /// CNC machine (e.g. CENTATEQ)
     /// </summary>
-    Cnc,
-
-    /// <summary>
-    /// Edgeband machine (e.g. EDGETEQ)
-    /// </summary>
-    Edgebanding
+    Cnc
 }
