73bb251a43e3518cf17c6e0726fdd1f03bfdfdbd
peter-lorincz-duerr <peter.lorincz@cadrantechnologies.eu>
2025-11-04 16:31:59 +0200

    add get and delete edgebandtype allocation request objects (#468)
    * add get and delete request objects
    
    * delete get object
    
    ---------
    
    Co-authored-by: Peter <peter.lorincz@principal33.com>

---
+++ Applications/MaterialManager/Contracts/Delete/EdgebandTypeAllocationDelete.cs
@@ -0,0 +1,8 @@
+﻿using HomagConnect.MaterialManager.Contracts.Material.Edgebands;
+
+namespace HomagConnect.MaterialManager.Contracts.Delete;
+
+/// <summary>
+/// Delete edgeband type allocation.
+/// </summary>
+public class EdgebandTypeAllocationDelete : EdgebandTypeAllocationRequestBase { }
