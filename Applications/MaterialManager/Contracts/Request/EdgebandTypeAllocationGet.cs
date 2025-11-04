d6a6eb7fa3a51804fd03c3c6a01cf9e4b21ba345
Peter <peter.lorincz@principal33.com>
2025-11-04 10:11:02 +0200

    add get and delete request objects

---
+++ Applications/MaterialManager/Contracts/Request/EdgebandTypeAllocationGet.cs
@@ -0,0 +1,8 @@
+﻿using HomagConnect.MaterialManager.Contracts.Material.Edgebands;
+
+namespace HomagConnect.MaterialManager.Contracts.Request;
+
+/// <summary>
+/// Edgeband type allocation get request.
+/// </summary>
+public class EdgebandTypeAllocationGet : EdgebandTypeAllocationRequestBase { }
