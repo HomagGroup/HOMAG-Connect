74fe4eebf43511c0a0fc8bd5340ce8f467b6e55a
Peter <peter.lorincz@principal33.com>
2025-11-04 15:49:27 +0200

    delete get object

--- Applications/MaterialManager/Contracts/Request/EdgebandTypeAllocationGet.cs
+++
@@ -1,8 +0,0 @@
-﻿using HomagConnect.MaterialManager.Contracts.Material.Edgebands;
-
-namespace HomagConnect.MaterialManager.Contracts.Request;
-
-/// <summary>
-/// Edgeband type allocation get request.
-/// </summary>
-public class EdgebandTypeAllocationGet : EdgebandTypeAllocationRequestBase { }
