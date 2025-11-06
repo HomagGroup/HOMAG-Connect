14bc20829518d3340d7934da4110b46501004751
Oana Sabadas <oana.sabadas@accesa.eu>
2025-11-06 15:11:06 +0200

    update designer accesibility lavel to initial values

--- Base/HomagConnect.Base.Contracts/Enumerations/WorkstationGroupDisplayName.Designer.cs
+++ Base/HomagConnect.Base.Contracts/Enumerations/WorkstationGroupDisplayName.Designer.cs
@@ -22,7 +22,7 @@ namespace HomagConnect.Base.Contracts.Enumerations {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class WorkstationGroupDisplayName {
+    public class WorkstationGroupDisplayName {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace HomagConnect.Base.Contracts.Enumerations {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HomagConnect.Base.Contracts.Enumerations.WorkstationGroupDisplayName", typeof(WorkstationGroupDisplayName).Assembly);
