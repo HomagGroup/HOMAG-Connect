6768ee1fcf34b90cb9de18e45b4daa60e77fc5c6
Adrian Liehner <145965882+aldfhg@users.noreply.github.com>
2025-11-07 08:45:40 +0100

    feat: implement changes to BoardEntityCreatedEvent (#474)

---
+++ Applications/MaterialManager/Contracts/Events/Material/Enums/UpsertAction.cs
@@ -0,0 +1,18 @@
+﻿namespace HomagConnect.MaterialManager.Contracts.Events.Material.Enums
+{
+    /// <summary>
+    /// Specifies the action performed during an upsert operation.
+    /// </summary>
+    public enum UpsertAction
+    {
+        /// <summary>
+        /// Indicates that a new material was created.
+        /// </summary>
+        Created,
+
+        /// <summary>
+        /// Indicates that an existing material was updated.
+        /// </summary>
+        Updated
+    }
+}
