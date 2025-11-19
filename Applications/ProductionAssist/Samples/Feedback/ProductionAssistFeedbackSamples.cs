fda9324d25036f76be63042e3274057979f98961
Claudiu Toderas <claudiu.toderas@accesa.eu>
2025-11-19 10:31:55 +0200

    Adjust GetWorkstations method

--- Applications/ProductionAssist/Samples/Feedback/ProductionAssistFeedbackSamples.cs
+++ Applications/ProductionAssist/Samples/Feedback/ProductionAssistFeedbackSamples.cs
@@ -1,6 +1,4 @@
-﻿using HomagConnect.Base.Extensions;
-using HomagConnect.ProductionAssist.Contracts.Feedback.Interfaces;
-using HomagConnect.ProductionManager.Contracts;
+﻿using HomagConnect.ProductionAssist.Contracts.Feedback.Interfaces;
 
 namespace HomagConnect.ProductionAssist.Samples.Feedback
 {
@@ -9,16 +7,6 @@ namespace HomagConnect.ProductionAssist.Samples.Feedback
     /// </summary>
     public static class ProductionAssistFeedbackSamples
     {
-        /// <summary>
-        /// Sample showing how to retrieve the list of configured feedback workstations.
-        /// </summary>
-        public static async Task GetWorkstations(IProductionAssistFeedbackClient client)
-        {
-            var response = await client.GetWorkstations();
-
-            response.Trace();
-        }
-
         /// <summary>
         /// Sample showing how to report a production entity as finished.
         /// </summary>
