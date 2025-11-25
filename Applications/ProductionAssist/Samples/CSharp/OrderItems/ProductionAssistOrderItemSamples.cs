81bf0f654435f0b3e62854031dc50159e12089ee
Claudiu Toderas <claudiu.toderas@accesa.eu>
2025-11-25 10:46:04 +0200

    Add docu

---
+++ Applications/ProductionAssist/Samples/CSharp/OrderItems/ProductionAssistOrderItemSamples.cs
@@ -0,0 +1,29 @@
+﻿using HomagConnect.Base.Extensions;
+using HomagConnect.ProductionAssist.Contracts;
+
+namespace HomagConnect.ProductionAssist.Samples.OrderItems
+{
+    /// <summary>
+    /// ProductionAssist orderItems samples.
+    /// </summary>
+    public static class ProductionAssistOrderItemSamples
+    {
+        /// <summary>
+        /// Sample showing how to retrieve order items.
+        /// </summary>
+        public static async Task GetOrderItemsWorkstations(IProductionAssistClient client)
+        {
+            var identifiers =new string[]
+            {
+                "orderItem1Identifier",
+                "orderItem2Identifier"
+            };
+
+            //Get the data  
+            var response = await client.GetOrderItems(identifiers);
+
+            //use the retrieved data
+            response.Trace();
+        }
+    }
+}
---
+++ Applications/ProductionAssist/Samples/CSharp/OrderItems/Readme.md
@@ -0,0 +1,25 @@
+# Access data using the HOMAG Connect Client
+
+### 1. Get Workstations
+With the HOMAG Connect Client, the orderItems can be retrieved from productionAssist for further programmatic evaluation.
+
+<strong>Example:</strong>
+
+```c#
+// Create new instance of the productionAssist client:
+  var client = new ProductionAssistClient(subscriptionId, authorizationKey);
+  var identifiers =new string[]
+  {
+      "orderItem1Identifier",
+      "orderItem2Identifier"
+  };
+
+  //Get the data  
+  var response = await client.GetOrderItems(identifiers);
+
+  //use the retrieved data
+  response.Trace();
+``` 
+
+The response is a list of [Workstation](../../Contracts/Workstation.cs) which exposes basic information about the workstation
+The sample code can be found at [ProductionAssist Samples ](../../Samples/ProductionAssistSamples.cs).
