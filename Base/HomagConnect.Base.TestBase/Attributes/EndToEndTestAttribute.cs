3b855caaa98acf51a33327d3816d5827e37f7930
Robin Katzenberger <141736258+HOMAG-Robin-Katzenberger@users.noreply.github.com>
2025-12-04 10:54:14 +0100

    feat: add test attributes (#506)

---
+++ Base/HomagConnect.Base.TestBase/Attributes/EndToEndTestAttribute.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HomagConnect.Base.TestBase.Attributes;
+
+/// <summary>
+/// Test Attribute to comment to link to an Area
+/// </summary>
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
+public sealed class EndToEndTestAttribute : TestCategoryBaseAttribute
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="T:Microsoft.VisualStudio.TestTools.UnitTesting.TestCategoryAttribute" />
+    /// class
+    /// by applying the supplied category to the test.
+    /// </summary>
+    /// <param name="area">The area to be applied.</param>
+    public EndToEndTestAttribute(string area)
+    {
+        var list = new List<string> { "EndToEndTests" };
+
+        if (!string.IsNullOrWhiteSpace(area))
+        {
+            var areas = area.Split('.');
+
+            for (int i = 0; i < areas.Length; i++)
+            {
+                var areaName = new StringBuilder();
+
+                for (int j = 0; j <= i; j++)
+                {
+                    areaName.Append(areas[j] + ".");
+                }
+
+                list.Add("EndToEndTests." + areaName.ToString().TrimEnd('.'));
+            }
+        }
+
+        TestCategories = list;
+    }
+
+    /// <summary>
+    /// Gets the test categories that have been applied to the test.
+    /// </summary>
+    public override IList<string> TestCategories { get; }
+}
---
+++ Base/HomagConnect.Base.TestBase/Attributes/UnitTestAttribute.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HomagConnect.Base.TestBase.Attributes;
+
+/// <summary>
+/// Test Attribute to comment to link to an Area
+/// </summary>
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
+public sealed class UnitTestAttribute : TestCategoryBaseAttribute
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="T:Microsoft.VisualStudio.TestTools.UnitTesting.TestCategoryAttribute" />
+    /// class
+    /// by applying the supplied category to the test.
+    /// </summary>
+    /// <param name="area">The area to be applied.</param>
+    public UnitTestAttribute(string area)
+    {
+        var list = new List<string> { "UnitTests" };
+
+        if (!string.IsNullOrWhiteSpace(area))
+        {
+            var areas = area.Split('.');
+
+            for (int i = 0; i < areas.Length; i++)
+            {
+                var areaName = new StringBuilder();
+
+                for (int j = 0; j <= i; j++)
+                {
+                    areaName.Append(areas[j] + ".");
+                }
+
+                list.Add("UnitTests." + areaName.ToString().TrimEnd('.'));
+            }
+        }
+
+        TestCategories = list;
+    }
+
+    /// <summary>
+    /// Gets the test categories that have been applied to the test.
+    /// </summary>
+    public override IList<string> TestCategories { get; }
+}
