e295b49dd94d3374c1fbe2282d626ed7c9e119cf
peter-lorincz-duerr <peter.lorincz@cadrantechnologies.eu>
2025-12-17 11:44:07 +0200

    Feature/330009 edgebad type allocations crud (#510)
    * initial commit wip
    
    * wip 2
    
    * more in progress
    
    * add tests and samples
    
    * update postman
    
    * code cleanup
    
    * do not compare edgebandcode yet
    
    * add brackets to ifs
    
    * add retry to update test
    
    * Revert "add retry to update test"
    
    This reverts commit e0224c2a375c6175a8b2e30109e9b7595a9ea7c1.
    
    * add retry to update allocation test
    
    * refacor create edgeband type test
    
    * fix and refactor tests
    
    ---------
    
    Co-authored-by: Peter <peter.lorincz@principal33.com>

---
+++ Applications/MaterialManager/Tests/Update/Allocations/UpdateEdgebandTypeAllocationTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+
+using HomagConnect.MaterialManager.Client;
+using HomagConnect.MaterialManager.Contracts.Update;
+
+namespace HomagConnect.MaterialManager.Tests.Update.Allocations
+{
+    /// <summary />
+    [TestClass]
+    [TestCategory("MaterialManager")]
+    public class UpdateEdgebandTypeAllocationTests : MaterialManagerTestBase
+    {
+        /// <summary>
+        /// MaterialManagerClientMaterialEdgebands
+        /// </summary>
+        protected MaterialManagerClientMaterialEdgebands MaterialManagerClientMaterialEdgebands { get; private set; } = null!;
+
+        /// <summary>
+        /// Setup method to initialize the MaterialManagerClientMaterialEdgebands client.
+        /// </summary>
+        [TestInitialize]
+        public async Task Setup()
+        {
+            MaterialManagerClientMaterialEdgebands = GetMaterialManagerClient().Material.Edgebands;
+            await EnsureEdgebandTypeExist(EdgebandCode);
+        }
+
+        /// <summary>
+        /// UpdateEdgebandTypeAllocation_NoException
+        /// </summary>
+        [TestMethod]
+        [DataRow("comments", "createdBy", "source", "workstation", 10.0, "customer", "Allocation1", "project", 5.0)]
+        public async Task UpdateEdgebandTypeAllocation_NoException(string comments, string createdBy, string source, string workstation, double allocatedLength,
+            string customer, string order, string project, double usedLength)
+        {
+            // Arrange
+            await EdgebandType_CreateEdgebandTypeAllocation_Cleanup(MaterialManagerClientMaterialEdgebands, EdgebandCode, customer, order, project);
+
+            var requestEdgebandTypeAllocation = CreateEdgebandTypeAllocationRequest(EdgebandCode, comments, createdBy, source, workstation, allocatedLength, customer, order, project, usedLength);
+
+            await MaterialManagerClientMaterialEdgebands.CreateEdgebandTypeAllocation(requestEdgebandTypeAllocation);
+
+            // Act
+            await MaterialManagerClientMaterialEdgebands.UpdateEdgebandTypeAllocation(new EdgebandTypeAllocationUpdate
+            {
+                AllocatedLength = 5,
+                Customer = customer,
+                EdgebandCode = EdgebandCode,
+                Order = order,
+                Project = project,
+            });
+
+            // Assert
+            await RetryAssertAsync(async () =>
+            {
+                var result = await MaterialManagerClientMaterialEdgebands.GetEdgebandTypeAllocation(order, customer, project, EdgebandCode);
+                result.AllocatedLength.Should().Be(5);
+            });
+
+            await EdgebandType_CreateEdgebandTypeAllocation_Cleanup(MaterialManagerClientMaterialEdgebands, EdgebandCode, customer, order, project);
+        }
+    }
+}
