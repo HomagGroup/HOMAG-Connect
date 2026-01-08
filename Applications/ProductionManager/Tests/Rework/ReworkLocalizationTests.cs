c5ca3a7ba5106bf1f7fd81be2cf383a007d5494b
Boris Wehrle (HOMAG) <141398900+HOMAG-Boris-Wehrle@users.noreply.github.com>
2026-01-08 11:02:53 +0100

    FluentAssertions removed

--- Applications/ProductionManager/Tests/Rework/ReworkLocalizationTests.cs
+++ Applications/ProductionManager/Tests/Rework/ReworkLocalizationTests.cs
@@ -1,10 +1,9 @@
 ﻿using System.Globalization;
 
-using FluentAssertions;
-
 using HomagConnect.Base.Extensions;
 using HomagConnect.Base.TestBase.Attributes;
 using HomagConnect.ProductionManager.Contracts.Rework;
+using Shouldly;
 
 // ReSharper disable StringLiteralTypo
 
@@ -23,10 +22,10 @@ namespace HomagConnect.ProductionManager.Tests.Rework
             var rework = new Contracts.Rework.Rework();
             var propertyDisplayNames = rework.GetPropertyDisplayNames(culture);
 
-            propertyDisplayNames.Should().NotBeEmpty(
+            propertyDisplayNames.ShouldNotBeEmpty(
                 "because Rework properties should have localized display names");
 
-            propertyDisplayNames[nameof(rework.CapturedAt)].Should().Be("Erfasst", because: "the CapturedAt property should be localized as 'Erfasst' in German");
+            propertyDisplayNames[nameof(rework.CapturedAt)].ShouldBe("Erfasst", "the CapturedAt property should be localized as 'Erfasst' in German");
 
             propertyDisplayNames.Trace();
         }
@@ -37,9 +36,9 @@ namespace HomagConnect.ProductionManager.Tests.Rework
         {
             var displayNames = EnumExtensions.GetDisplayNames<ReworkCategory>(CultureInfo.GetCultureInfo("de"));
 
-            displayNames.Should().NotBeEmpty(
+            displayNames.ShouldNotBeEmpty(
                 "because ReworkCategory enum should have localized display names");
-            displayNames[ReworkCategory.Dividing].Should().Be("Aufteilen",
+            displayNames[ReworkCategory.Dividing].ShouldBe("Aufteilen",
                 "because ReworkCategory.Dividing should be localized as 'Aufteilen' in German");
 
             displayNames.Trace();
@@ -53,10 +52,10 @@ namespace HomagConnect.ProductionManager.Tests.Rework
             var creationDetails = new CreationDetails();
             var propertyDisplayNames = creationDetails.GetPropertyDisplayNames(culture);
 
-            propertyDisplayNames.Should().NotBeEmpty(
+            propertyDisplayNames.ShouldNotBeEmpty(
                 "because CreationDetails properties should have localized display names");
 
-            propertyDisplayNames[nameof(creationDetails.Comment)].Should().Be("Notizen", because: "the Comment property should be localized as 'Notizen' in German");
+            propertyDisplayNames[nameof(creationDetails.Comment)].ShouldBe("Notizen", "the Comment property should be localized as 'Notizen' in German");
 
             propertyDisplayNames.Trace();
         }
@@ -67,9 +66,9 @@ namespace HomagConnect.ProductionManager.Tests.Rework
         {
             var displayNames = EnumExtensions.GetDisplayNames<ReworkReason>(CultureInfo.GetCultureInfo("de"));
 
-            displayNames.Should().NotBeEmpty(
+            displayNames.ShouldNotBeEmpty(
                 "because ReworkReason enum should have localized display names");
-            displayNames[ReworkReason.Tearouts].Should().Be("Ausrisse",
+            displayNames[ReworkReason.Tearouts].ShouldBe("Ausrisse",
                 "because ReworkReason.Tearouts should be localized as 'Ausrisse' in German");
 
             displayNames.Trace();
@@ -83,10 +82,10 @@ namespace HomagConnect.ProductionManager.Tests.Rework
             var rejectionDetails = new RejectionDetails();
             var propertyDisplayNames = rejectionDetails.GetPropertyDisplayNames(culture);
 
-            propertyDisplayNames.Should().NotBeEmpty(
+            propertyDisplayNames.ShouldNotBeEmpty(
                 "because RejectionDetails properties should have localized display names");
 
-            propertyDisplayNames[nameof(rejectionDetails.Comment)].Should().Be("Notizen", because: "the Comment property should be localized as 'Notizen' in German");
+            propertyDisplayNames[nameof(rejectionDetails.Comment)].ShouldBe("Notizen", "the Comment property should be localized as 'Notizen' in German");
 
             propertyDisplayNames.Trace();
         }
@@ -97,9 +96,9 @@ namespace HomagConnect.ProductionManager.Tests.Rework
         {
             var displayNames = EnumExtensions.GetDisplayNames<ReworkState>(CultureInfo.GetCultureInfo("de"));
 
-            displayNames.Should().NotBeEmpty(
+            displayNames.ShouldNotBeEmpty(
                 "because ReworkState enum should have localized display names");
-            displayNames[ReworkState.Rejected].Should().Be("Verweigert",
+            displayNames[ReworkState.Rejected].ShouldBe("Verweigert",
                 "because ReworkState.Rejected should be localized as 'Verweigert' in German");
 
             displayNames.Trace();
