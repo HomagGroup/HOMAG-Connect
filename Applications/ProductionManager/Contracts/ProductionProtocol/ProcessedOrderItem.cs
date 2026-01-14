be6de1cb6528bc49023f7679d06e22e1f9a3da73
joBueche <139100771+joBueche@users.noreply.github.com>
2026-01-14 14:41:21 +0100

    add resources for all Properties of the  models, that reflect endpoints of productionProtocol (#544)

--- Applications/ProductionManager/Contracts/ProductionProtocol/ProcessedOrderItem.cs
+++ Applications/ProductionManager/Contracts/ProductionProtocol/ProcessedOrderItem.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 
 namespace HomagConnect.ProductionManager.Contracts.ProductionProtocol
 {
@@ -11,29 +12,34 @@ namespace HomagConnect.ProductionManager.Contracts.ProductionProtocol
         /// Gets or sets the id of the processed OrderItem
         /// </summary>
         [JsonProperty(Order = 10)]
+        [Display(ResourceType = typeof(ProductionProtocolPropertyDisplayNames), Name = nameof(Id))]
         public string? Id { get; set; }
 
         /// <summary>
         /// Gets or sets the description of the processed OrderItem
         /// </summary>
         [JsonProperty(Order = 11)]
+        [Display(ResourceType = typeof(ProductionProtocolPropertyDisplayNames), Name = nameof(Description))]
         public string? Description { get; set; }
 
         /// <summary>
         /// Gets or sets the quantity processed.
         /// </summary>
         [JsonProperty(Order = 12)]
+        [Display(ResourceType = typeof(ProductionProtocolPropertyDisplayNames), Name = nameof(Quantity))]
         public int? Quantity { get; set; }
 
         /// <summary>
         /// Gets or sets the name of the Order in which the OrderItem was defined.
         /// </summary>
         [JsonProperty(Order = 13)]
+        [Display(ResourceType = typeof(ProductionProtocolPropertyDisplayNames), Name = nameof(OrderName))]
         public string? OrderName { get; set; }
 
         /// <summary>
         /// Gets or sets the OrderId in which the OrderItem was defined.
         /// </summary>
+        [Display(ResourceType = typeof(ProductionProtocolPropertyDisplayNames), Name = nameof(OrderId))]
         [JsonProperty(Order = 14)]
         public Guid? OrderId { get; set; }
 
@@ -41,6 +47,7 @@ namespace HomagConnect.ProductionManager.Contracts.ProductionProtocol
         /// Gets or sets the name of the Customer name.
         /// </summary>
         [JsonProperty(Order = 15)]
+        [Display(ResourceType = typeof(ProductionProtocolPropertyDisplayNames), Name = nameof(CustomerName))]
         public string? CustomerName { get; set; }
     }
 }
--- Applications/ProductionManager/Contracts/ProductionProtocol/ProcessedPartCnc.cs
+++ Applications/ProductionManager/Contracts/ProductionProtocol/ProcessedPartCnc.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 
 namespace HomagConnect.ProductionManager.Contracts.ProductionProtocol;
 
@@ -11,29 +12,34 @@ public class ProcessedPartCnc : ProcessedPart
     /// Gets or sets the duration of the CNC program execution for this part.
     /// </summary>
     [JsonProperty(Order = 31)]
+    [Display(ResourceType = typeof(ProductionProtocolPropertyDisplayNames), Name = nameof(ProgramDuration))]
     public TimeSpan? ProgramDuration { get; set; }
 
     /// <summary>
     /// Gets or sets the name of the CNC program executed for this part.
     /// </summary>
     [JsonProperty(Order = 32)]
+    [Display(ResourceType = typeof(ProductionProtocolPropertyDisplayNames), Name = nameof(ProgramName))]
     public string? ProgramName { get; set; }
 
     /// <summary>
     /// Gets or sets the preview image for this part.
     /// </summary>
     [JsonProperty(Order = 33)]
+    [Display(ResourceType = typeof(ProductionProtocolPropertyDisplayNames), Name = nameof(Preview))]
     public Uri? Preview { get; set; }
 
     /// <summary>
     /// Gets or sets the Timestamp when the CNC program execution started.
     /// </summary>
     [JsonProperty(Order = 34)]
+    [Display(ResourceType = typeof(ProductionProtocolPropertyDisplayNames), Name = nameof(StartedAt))]
     public DateTimeOffset? StartedAt { get; set; }
 
     /// <summary>
     /// Gets or sets the Timestamp when the CNC program execution completed.
     /// </summary>
     [JsonProperty(Order = 35)]
+    [Display(ResourceType = typeof(ProductionProtocolPropertyDisplayNames), Name = nameof(CompletedAt))]
     public DateTimeOffset? CompletedAt { get; set; }
 }
