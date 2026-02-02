840fedefbf9530189e0a200a2842696c9e78578c
Oana Sabadas <oana.sabadas@accesa.eu>
2026-02-02 11:04:45 +0200

    Add Rework Events for creating and updating status

---
+++ Applications/ProductionManager/Contracts/Events/Rework/ReworkStatusChangedEvent.cs
@@ -0,0 +1,29 @@
+﻿using HomagConnect.Base.Contracts.Attributes;
+using HomagConnect.Base.Contracts.Events;
+using HomagConnect.ProductionManager.Contracts.Rework;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomagConnect.ProductionManager.Contracts.Events.Rework
+{
+    /// <summary>
+    /// Represents an event that is triggered when the status of a rework changes in the Production Manager.
+    /// </summary>
+    [AppEvent(nameof(ProductionManager) + "." + nameof(Rework) + "." + nameof(ReworkStatusChangedEvent))]
+    public class ReworkStatusChangedEvent : AppEvent
+    {
+        /// <summary>
+        /// Gets or sets the current state of the rework.
+        /// </summary>
+        [Required]
+        [JsonProperty(Order = 10)]
+        public ReworkState State { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user or system that changed the rework status.
+        /// </summary>
+        [Required]
+        [JsonProperty(Order = 11)]
+        public string? StatusChangedBy { get; set; }
+    }
+}
