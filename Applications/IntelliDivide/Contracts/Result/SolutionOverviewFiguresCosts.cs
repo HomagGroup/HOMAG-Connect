b4336555d9b1e4ca1baeea6ec7a50fada1271655
Andrei Manea <163002468+andrei-m-acc@users.noreply.github.com>
2026-02-02 15:26:10 +0200

    Added properties for total and production costs (#592)

--- Applications/IntelliDivide/Contracts/Result/SolutionOverviewFiguresCosts.cs
+++ Applications/IntelliDivide/Contracts/Result/SolutionOverviewFiguresCosts.cs
@@ -35,6 +35,24 @@ public class SolutionOverviewFiguresCosts
     public double? MaterialCostsPerPart { get; set; }
 
     /// <summary>
+    /// Gets the production costs in the currency of the subscription.
+    /// </summary>
+    [JsonProperty(Order = 4)]
+    public double? ProductionCosts { get; set; }
+
+    /// <summary>
+    /// Gets the total costs in the currency of the subscription.
+    /// </summary>
+    [JsonProperty(Order = 5)]
+    public double? TotalCosts { get; set; }
+
+    /// <summary>
+    /// Gets the total costs per part in the currency of the subscription.
+    /// </summary>
+    [JsonProperty(Order = 6)]
+    public double? TotalCostsPerPart { get; set; }
+
+    /// <summary>
     /// Gets or sets the additional properties configured in the application.
     /// </summary>
     [JsonProperty(Order = 80)]
