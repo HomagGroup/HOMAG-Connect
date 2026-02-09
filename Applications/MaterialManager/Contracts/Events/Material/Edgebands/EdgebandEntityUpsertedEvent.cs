3124f7457113423e7ec8785173d0772eaee75a88
Adrian Liehner <145965882+aldfhg@users.noreply.github.com>
2026-02-09 14:32:32 +0100

    Feature/add board type event add edgeband type event (#464)
    * feat: add BoardTypeCreatedEvent and EdgebandTypeCreatedEvent and tests
    
    * feat: add and update events and tests
    
    * merged main
    added 3 new events
    
    * adjust tests after merge main
    
    * Add also EdbandEntityUpsertedEvent
    
    * adjus ttests
    
    * rename file
    
    * renaming
    
    ---------
    
    Co-authored-by: Büche, Johannes <Johannes.Bueche@homag.com>

---
+++ Applications/MaterialManager/Contracts/Events/Material/Edgebands/EdgebandEntityUpsertedEvent.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using HomagConnect.Base.Contracts.Attributes;
+using HomagConnect.Base.Contracts.Enumerations;
+using HomagConnect.Base.Contracts.Events;
+using HomagConnect.MaterialManager.Contracts.Material.Edgebands;
+
+using Newtonsoft.Json;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+namespace HomagConnect.MaterialManager.Contracts.Events.Material.Edgebands
+{
+    /// <summary>
+    /// Event that occurs when a board entity has been upserted.
+    /// </summary>
+    [AppEvent(nameof(MaterialManager) + "." + nameof(Material) + "." + nameof(Edgebands) + "." + nameof(EdgebandEntityUpsertedEvent))]
+    public class EdgebandEntityUpsertedEvent : AppEvent
+    {
+        /// <summary>
+        /// Gets or sets the action performed during the upsert operation.
+        /// </summary>
+        public UpsertAction Action { get; set; }
+
+        /// <summary>
+        /// Gets or sets the BoardEntity that has been upserted.
+        /// </summary>
+        [Required]
+        [JsonProperty(Order = 20)]
+        public EdgebandEntity EdgebandEntity { get; set; }
+    }
+}
