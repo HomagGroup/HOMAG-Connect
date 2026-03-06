9ce805bbbb843075da6446e97be2aabac80cddce
HOMAG-Alexandru-Marcu <alexandru.marcu@cadrantechnologies.eu>
2026-03-06 17:41:07 +0200

    feat: textures pagination (#643)

---
+++ Applications/MaterialManager/Contracts/Surfaces/Textures/PagedTextureResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace HomagConnect.MaterialManager.Contracts.Surfaces.Textures;
+
+/// <summary>
+/// Result of a paged texture query using continuation token pagination.
+/// </summary>
+public class PagedTextureResult
+{
+    /// <summary>
+    /// The textures in the current page.
+    /// </summary>
+    public IReadOnlyList<Texture> Textures { get; set; } = [];
+
+    /// <summary>
+    /// The continuation token for fetching the next page.
+    /// Null when there are no more pages.
+    /// </summary>
+    public string? ContinuationToken { get; set; }
+}
