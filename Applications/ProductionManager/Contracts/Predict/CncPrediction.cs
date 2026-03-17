550c86fa1f827e977fd597682d80994b5bb7ad9e
Boris Wehrle (HOMAG) <141398900+HOMAG-Boris-Wehrle@users.noreply.github.com>
2026-03-17 16:35:32 +0100

    Predict data contracts documented

--- Applications/ProductionManager/Contracts/Predict/CncPrediction.cs
+++ Applications/ProductionManager/Contracts/Predict/CncPrediction.cs
@@ -1,24 +1,33 @@
 ﻿using System.Runtime.Serialization;
 using Newtonsoft.Json;
 
-namespace HomagConnect.ProductionManager.Contracts.Predict
+namespace HomagConnect.ProductionManager.Contracts.Predict;
+
+/// <summary>
+/// Represents a CNC duration prediction.
+/// Inherits the predicted duration, minimum duration, maximum duration, and prediction method from <see cref="PredictionBase" />.
+/// </summary>
+/// <example>
+/// {
+///   "duration": "00:12:30",
+///   "durationMin": "00:10:00",
+///   "durationMax": "00:15:00",
+///   "predictionBase": [
+///     { "dataSourceType": "1-001-01-0001", "duration": "00:12:30", "durationMin": "00:10:00", "durationMax": "00:15:00" }
+///   ]
+/// }
+/// </example>
+public class CncPrediction : PredictionBase
 {
     /// <summary>
-    /// CNC Prediction
+    /// Gets or sets the machine-specific prediction data used as the basis for the aggregated CNC prediction.
+    /// Machine identifiers use the format <c>x-xxx-xx-xxxx</c>.
     /// </summary>
-    public class CncPrediction : PredictionBase
-    {
-        /// <summary>
-        /// Machines on which the prediction is based (in format "x-xxx-xx-xxxx").
-        /// </summary>
-        [JsonProperty(Order = 6)]
-        public IEnumerable<PredictionBaseData> PredictionBase { get; set; } = [];
-
-        #region IExtensibleDataObject Members
-
-        /// <inheritdoc />
-        public ExtensionDataObject? ExtensionData { get; set; }
-
-        #endregion
-    }
+    /// <example>
+    /// [
+    ///   { "dataSourceType": "1-001-01-0001", "duration": "00:12:30", "durationMin": "00:10:00", "durationMax": "00:15:00" }
+    /// ]
+    /// </example>
+    [JsonProperty(Order = 6)]
+    public IEnumerable<PredictionBaseData> PredictionBase { get; set; } = [];
 }
