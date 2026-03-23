9810ba285466074656f035401aecfb0fcce54b7f
Andrei Manea <andrei.manea@accesa.eu>
2026-03-23 14:12:09 +0200

    Disabled tests until Gateway change is deployed

--- Applications/IntelliDivide/Tests/Requests/Cutting/CuttingOptimizationRequestUsingTemplateTests.cs
+++ Applications/IntelliDivide/Tests/Requests/Cutting/CuttingOptimizationRequestUsingTemplateTests.cs
@@ -1,3 +1,4 @@
+using HomagConnect.Base.TestBase.Attributes;
 using HomagConnect.IntelliDivide.Contracts.Common;
 using HomagConnect.IntelliDivide.Samples.Requests;
 using HomagConnect.IntelliDivide.Samples.Requests.Cutting.Template;
@@ -26,6 +27,7 @@ public class CuttingOptimizationRequestUsingTemplateTests : IntelliDivideTestBas
 
     /// <summary />
     [TestMethod]
+    [TemporaryDisabledOnServer(2026, 04, 06, "DF-Optimization")]
     public async Task CuttingRequest_Template_Excel_ImportOnly()
     {
         var intelliDivide = GetIntelliDivideClient();
