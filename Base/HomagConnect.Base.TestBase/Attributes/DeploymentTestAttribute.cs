cba3dddfa4237ee089deb953071048c340cec801
Boris Wehrle (HOMAG) <141398900+HOMAG-Boris-Wehrle@users.noreply.github.com>
2026-03-24 17:16:20 +0100

    TestPriority introduced

--- Base/HomagConnect.Base.TestBase/Attributes/DeploymentTestAttribute.cs
+++ Base/HomagConnect.Base.TestBase/Attributes/DeploymentTestAttribute.cs
@@ -1,49 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HomagConnect.Base.TestBase.Attributes;
 
 /// <summary>
-/// Test Attribute to comment to link to an Area
+/// Applies deployment-related categories to a test, including area-based categories and an optional <see cref="TestPriority" />.
 /// </summary>
 [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
 public sealed class DeploymentTestAttribute : TestCategoryBaseAttribute
 {
+    private const string _RootCategory = "DeploymentTests";
+
     /// <summary>
-    /// Initializes a new instance of the <see cref="T:Microsoft.VisualStudio.TestTools.UnitTesting.TestCategoryAttribute" />
-    /// class
-    /// by applying the supplied category to the test.
+    /// Initializes a new instance of the <see cref="DeploymentTestAttribute" /> class.
     /// </summary>
-    /// <param name="area">The area to be applied.</param>
-    public DeploymentTestAttribute(string area)
+    /// <param name="area">The area path used to generate hierarchical deployment test categories.</param>
+    /// <param name="priority">The optional priority category to apply to the test.</param>
+    public DeploymentTestAttribute(string area, TestPriority priority = TestPriority.Undefined)
     {
-        var list = new List<string> { "DeploymentTests" };
+        TestCategories = BuildTestCategories(area, priority);
+    }
+
+    /// <inheritdoc />
+    public override IList<string> TestCategories { get; }
+
+    private static List<string> BuildTestCategories(string area, TestPriority priority)
+    {
+        var categories = new List<string> { _RootCategory };
 
         if (!string.IsNullOrWhiteSpace(area))
         {
             var areas = area.Split('.');
 
-            for (int i = 0; i < areas.Length; i++)
-            {
-                var areaName = new StringBuilder();
-
-                for (int j = 0; j <= i; j++)
-                {
-                    areaName.Append(areas[j] + ".");
-                }
-
-                list.Add("DeploymentTests." + areaName.ToString().TrimEnd('.'));
-            }
+            categories.AddRange(areas.Select((t, i) => $"{_RootCategory}.{string.Join('.', areas, 0, i + 1)}"));
         }
+        
+        categories.Add($"{_RootCategory}.Priority.{priority}");
 
-        TestCategories = list;
+        return categories;
     }
-
-    /// <summary>
-    /// Gets the test categories that have been applied to the test.
-    /// </summary>
-    public override IList<string> TestCategories { get; }
 }
