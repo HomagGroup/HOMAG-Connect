5e659d7240754836f388019b7f87c9e9a1764751
Andrei Manea <andrei.manea@accesa.eu>
2026-04-01 14:27:40 +0300

    Enabled test after hotfix deploy

--- Applications/IntelliDivide/Tests/Optimizations/OptimizationsCandidateEvaluationTests.cs
+++ Applications/IntelliDivide/Tests/Optimizations/OptimizationsCandidateEvaluationTests.cs
@@ -55,7 +55,6 @@ public class OptimizationsCandidateEvaluationTests : IntelliDivideTestBase
     }
 
     [TestMethod]
-    [TemporaryDisabledOnServer(2026, 05, 01, "DF-Optimization")]
     public async Task Optimizations_Nesting_GetFirstOptimizationAndEvaluate()
     {
         var solutions = await GetSampleSolutionDetails(OptimizationType.Nesting);
