ee2f82a5bea60f537ea45ab8b5ed3131fae1e4e4
Peter <peter.lorincz@cadrantechnologies.eu>
2026-04-21 10:13:13 +0300

    add LocationType to StorageLocation

--- Base/HomagConnect.Base.Contracts/StorageLocation.cs
+++ Base/HomagConnect.Base.Contracts/StorageLocation.cs
@@ -9,17 +9,23 @@ public class StorageLocation
     /// Gets or sets the barcode of the compartment.
     /// </summary>
     /// <example>COMP-0004711</example>
-    public string Barcode { get; set; }
+    public string Barcode { get; set; } = string.Empty;
 
     /// <summary>
     /// Gets or sets the unique identifier of the storage location.
     /// </summary>
     /// <example>LOC-01-02-03</example>
-    public string LocationId { get; set; }
+    public string LocationId { get; set; } = string.Empty;
 
     /// <summary>
     /// Gets or sets the display name of the storage location.
     /// </summary>
     /// <example>Main Buffer 03</example>
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the type of the storage location, represented as an integer.
+    /// </summary>
+    /// <example>1</example>
+    public int LocationType { get; set; }
 }
