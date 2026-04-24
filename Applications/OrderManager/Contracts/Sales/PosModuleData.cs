b9450e4e9bf6bce5e4434f74db9ca25646eec3f5
Jochen Kalmbach <35481830+JochenKalmbachHomag@users.noreply.github.com>
2026-04-24 08:02:32 +0200

    First version of sales articles and masterData (#732)

---
+++ Applications/OrderManager/Contracts/Sales/PosModuleData.cs
@@ -0,0 +1,43 @@
+using Newtonsoft.Json;
+
+namespace HomagConnect.OrderManager.Contracts.Sales
+{
+    /// <summary>
+    /// Represents a module within an article, including its sub modules and attributes.
+    /// </summary>
+    public class PosModuleData
+    {
+        /// <summary>
+        /// An unique id of this module; this is also stable across multiple calculation calls
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id { get; set; } = null!;
+
+        /// <summary>
+        /// The name of this module; this is NOT unique
+        /// It is internally needed to calculate the article
+        /// See also <see cref="MDModuleInfo"/> for more info about modules
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// possible sub modules
+        /// </summary>
+        [JsonProperty("modules")]
+        public IList<PosModuleData>? Modules { get; set; }
+
+        /// <summary>
+        /// These are the parameters of this module
+        /// </summary>
+        [JsonProperty("attributes")]
+        public IList<PosModuleAttribute>? Attributes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the additional properties configured in the application.
+        /// </summary>
+        [JsonExtensionData]
+        [JsonProperty(Order = 999)]
+        public IDictionary<string, object>? AdditionalProperties { get; set; }
+    }
+}
