732407a8d916c11655cc88a61ea803eedb58a5ae
Reichstetter, Heiko <Heiko.Reichstetter@homag.com>
2026-05-27 14:15:00 +0200

    add customer state

---
+++ Applications/OrderManager/Contracts/Customers/State.cs
@@ -0,0 +1,33 @@
+﻿namespace HomagConnect.OrderManager.Contracts.Customers
+{
+    /// <summary>
+    /// State
+    /// </summary>
+    public enum State
+    {
+        /// <summary>
+        /// Active
+        /// </summary>
+        Active,
+
+        /// <summary>
+        /// Registration is pending and not yet <see cref="Active"/>
+        /// </summary>
+        Pending,
+
+        /// <summary>
+        /// Blocked
+        /// </summary>
+        Blocked,
+
+        /// <summary>
+        /// Deactivated
+        /// </summary>
+        Deactivated,
+
+        /// <summary>
+        /// Archived
+        /// </summary>
+        Archived
+    }
+}
