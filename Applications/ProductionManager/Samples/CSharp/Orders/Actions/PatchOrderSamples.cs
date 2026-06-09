5577df8af6c68345c3ef682d43ecadc8fc99c8fa
Toderas Claudiu <47099441+claudiutoderas10@users.noreply.github.com>
2026-06-09 11:22:49 +0300

    Add Endpoint for PatchOrder (#788)
    Co-authored-by: Claudiu Toderas <claudiu.toderas@accesa.eu>

---
+++ Applications/ProductionManager/Samples/CSharp/Orders/Actions/PatchOrderSamples.cs
@@ -0,0 +1,33 @@
+﻿using HomagConnect.Base.Contracts;
+using HomagConnect.ProductionManager.Contracts;
+using HomagConnect.ProductionManager.Contracts.Orders;
+using Newtonsoft.Json.Linq;
+using System.Globalization;
+
+namespace HomagConnect.ProductionManager.Samples.Orders.Actions
+{
+    /// <summary>
+    /// Patch order samples
+    /// </summary>
+    public static class PatchOrderSamples
+    {
+        /// <summary>
+        /// Patch order
+        /// </summary>
+        /// <param name="productionManagerClient"></param>
+        /// <returns></returns>
+        public static async Task PatchOrder(IProductionManagerClient productionManagerClient)
+        {
+            string identifier = Guid.NewGuid().ToString(); // set existing order identifier (e.g. order number/ order id/ order externalNumber)
+
+            var patchData = PatchBuilder<OrderDetails>.For()
+                .Set(o => o.CustomerName, "Muster GmbH")
+                .Set(o => o.DeliveryDatePlanned, DateTime.Parse("2026-09-15T00:00:00Z", CultureInfo.CurrentCulture))
+                .Set(o => o.Email, null)
+                .Build();
+
+            var jPatchData = JObject.FromObject(patchData);
+            await productionManagerClient.PatchOrder(identifier, jPatchData);
+        }
+    }
+}
