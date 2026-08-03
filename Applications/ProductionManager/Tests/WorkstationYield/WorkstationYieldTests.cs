811d4a16bdfd58e822c7c47dc0a6fcdf3822e599
joBueche <139100771+joBueche@users.noreply.github.com>
2026-08-03 10:16:37 +0200

    Feature/refinement workstation yield (#824)

--- Applications/ProductionManager/Tests/WorkstationYield/WorkstationYieldTests.cs
+++ Applications/ProductionManager/Tests/WorkstationYield/WorkstationYieldTests.cs
@@ -80,20 +80,19 @@ namespace HomagConnect.ProductionManager.Tests.WorkstationYield
                 new(ProductionItemType.Part, 100)
             };
 
-            var yield = new WorkstationsYield();
-            ((List<Contracts.WorkstationYield.WorkstationYield>)yield.Yields).Add(node1);
-            ((List<Contracts.WorkstationYield.WorkstationYield>)yield.Yields).Add(node2);
+            var yield = new List<Contracts.WorkstationYield.WorkstationYield>();
+            yield.Add(node1);
+            yield.Add(node2);
 
             yield.Trace();
 
             var serialized = JsonConvert.SerializeObject(yield, SerializerSettings.Default);
-            var deserialized = JsonConvert.DeserializeObject<WorkstationsYield>(serialized, SerializerSettings.Default);
+            var deserialized = JsonConvert.DeserializeObject<List<Contracts.WorkstationYield.WorkstationYield>>(serialized, SerializerSettings.Default);
 
             deserialized.ShouldNotBeNull();
-            deserialized.Yields.ShouldNotBeNull();
-            deserialized.Yields.Count().ShouldBe(2);
+            deserialized.Count().ShouldBe(2);
 
-            var deserializedNode1 = deserialized.Yields.First();
+            var deserializedNode1 = deserialized.First();
             deserializedNode1.Workstation.ShouldNotBeNull();
             deserializedNode1.Workstation.Id.ShouldBe(workstation1Id);
             deserializedNode1.Workstation.Name.ShouldBe("CNC Machine 1");
