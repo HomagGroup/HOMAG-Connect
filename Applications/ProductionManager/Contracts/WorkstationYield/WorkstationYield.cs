bb557fdb3edd1fb31705d796e7cf1c8c610c2091
joBueche <139100771+joBueche@users.noreply.github.com>
2026-08-07 08:30:03 +0200

    removed obsolete fields of workstation (#825)

--- Applications/ProductionManager/Contracts/WorkstationYield/WorkstationYield.cs
+++ Applications/ProductionManager/Contracts/WorkstationYield/WorkstationYield.cs
@@ -3,6 +3,7 @@ using HomagConnect.Base.Contracts.Interfaces;
 using HomagConnect.ProductionManager.Contracts.ProductionItems;
 
 using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 
 namespace HomagConnect.ProductionManager.Contracts.WorkstationYield;
 
@@ -15,18 +16,21 @@ public class WorkstationYield : ISupportsLocalizedSerialization
     /// Gets or sets the tapio machine ID
     /// </summary>
     [JsonProperty(Order = 20)]
+    [Display(ResourceType = typeof(WorkstationYieldPropertyNames), Name = nameof(Workstation))]
     public Workstation? Workstation { get; set; }
 
     /// <summary>
     /// Gets or sets the total count of parts on this workstation
     /// </summary>
     [JsonProperty(Order = 21)]
+    [Display(ResourceType = typeof(WorkstationYieldPropertyNames), Name = nameof(TotalCount))]
     public int TotalCount { get; set; }
     
     /// <summary>
     /// Gets or sets the distribution of parts per type on this workstation
     /// </summary>
     [JsonProperty(Order = 23)]
+    [Display(ResourceType = typeof(WorkstationYieldPropertyNames), Name = nameof(Yields))]
     public IEnumerable<KeyValuePair<ProductionItemType, int>> Yields { get; set; } = new List<KeyValuePair<ProductionItemType, int>>();
 
 }
