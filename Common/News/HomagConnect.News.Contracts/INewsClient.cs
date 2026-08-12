84c76267b72658e5c2994f53f7dd85f1c6125f65
Boris Wehrle (HOMAG) <141398900+HOMAG-Boris-Wehrle@users.noreply.github.com>
2026-08-12 13:02:48 +0200

    News contracts added

---
+++ Common/News/HomagConnect.News.Contracts/INewsClient.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace HomagConnect.News.Contracts;
+
+/// <summary>
+/// Defines endpoints for reading HOMAG Connect news.
+/// </summary>
+public interface INewsClient
+{
+    /// <summary>
+    /// Gets the latest news articles for the specified culture and tags.
+    /// </summary>
+    Task<IReadOnlyList<NewsArticle>> GetLatest(CultureInfo cultureInfo, IEnumerable<string> tags, int take, CancellationToken cancellationToken = default);
+}
---
+++ Common/News/HomagConnect.News.Contracts/NewsArticle.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+namespace HomagConnect.News.Contracts
+{
+    /// <summary>
+    /// Represents a news article.
+    /// </summary>
+    public sealed class NewsArticle
+    {
+        /// <summary>
+        /// Gets the unique identifier of the article.
+        /// </summary>
+        public required string Id { get; init; }
+
+        /// <summary>
+        /// Gets the article title.
+        /// </summary>
+        public required string Title { get; init; }
+
+        /// <summary>
+        /// Gets the URL to the full article.
+        /// </summary>
+        public required Uri Url { get; init; }
+
+        /// <summary>
+        /// Gets the article lead text.
+        /// </summary>
+        public required string Lead { get; init; }
+
+        /// <summary>
+        /// Gets the article category.
+        /// </summary>
+        public required string Category { get; init; }
+
+        /// <summary>
+        /// Gets the tags assigned to the article.
+        /// </summary>
+        public required string[] Tags { get; init; }
+
+        /// <summary>
+        /// Gets the publication date of the article.
+        /// </summary>
+        public required DateTimeOffset Date { get; init; }
+
+        /// <summary>
+        /// Gets or sets the culture of the article content.
+        /// </summary>
+        public required CultureInfo Culture { get; init; }
+    }
+}
