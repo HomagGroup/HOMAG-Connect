5a4e2f74be009da428467fadf2f2047ad73097ef
Boris Wehrle (HOMAG) <141398900+HOMAG-Boris-Wehrle@users.noreply.github.com>
2026-08-14 17:04:57 +0200

    Version renamed (#839)

--- Common/News/HomagConnect.News.Contracts/NewsArticle.cs
+++ Common/News/HomagConnect.News.Contracts/NewsArticle.cs
@@ -1,4 +1,4 @@
-﻿namespace HomagConnect.News.Contracts;
+namespace HomagConnect.News.Contracts;
 
 /// <summary>
 /// Represents a news article.
@@ -6,9 +6,39 @@
 public sealed class NewsArticle
 {
     /// <summary>
+    /// Gets the article category.
+    /// </summary>
+    public required string Category { get; init; }
+
+    /// <summary>
+    /// Gets the culture of the article content.
+    /// </summary>
+    public required string Culture { get; init; }
+
+    /// <summary>
+    /// Gets the publication date of the article.
+    /// </summary>
+    public required DateTimeOffset Date { get; init; }
+
+    /// <summary>
     /// Gets the unique identifier of the article.
     /// </summary>
     public required string Id { get; init; }
+
+    /// <summary>
+    /// Gets th URL of the first image in the article.
+    /// </summary>
+    public Uri? ImageUrl { get; set; }
+
+    /// <summary>
+    /// Gets the article lead text.
+    /// </summary>
+    public required string Lead { get; init; }
+
+    /// <summary>
+    /// Gets the tags assigned to the article.
+    /// </summary>
+    public required string[] Tags { get; init; }
 
     /// <summary>
     /// Gets the article title.
@@ -21,37 +51,7 @@ public sealed class NewsArticle
     public required Uri Url { get; init; }
 
     /// <summary>
-    /// Gets th URL of the first image in the article. 
+    /// Gets the version of the article.
     /// </summary>
-    public Uri? ImageUrl { get; set; }
-
-    /// <summary>
-    /// Gets the article lead text.
-    /// </summary>
-    public required string Lead { get; init; }
-
-    /// <summary>
-    /// Gets the article category.
-    /// </summary>
-    public required string Category { get; init; }
-
-    /// <summary>
-    /// Gets the tags assigned to the article.
-    /// </summary>
-    public required string[] Tags { get; init; }
-
-    /// <summary>
-    /// Gets the publication date of the article.
-    /// </summary>
-    public required DateTimeOffset Date { get; init; }
-
-    /// <summary>
-    /// Gets the culture of the article content.
-    /// </summary>
-    public required string Culture { get; init; }
-
-    /// <summary>
-    /// Gets the version number of the article.
-    /// </summary>
-    public int VersionNumber { get; init; } = 0;
+    public int Version { get; set; } = 1;
 }
