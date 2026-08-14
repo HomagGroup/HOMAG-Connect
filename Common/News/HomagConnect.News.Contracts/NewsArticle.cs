62d26078575e80beb39f03822a7dec3f03f36449
Boris Wehrle (HOMAG) <141398900+HOMAG-Boris-Wehrle@users.noreply.github.com>
2026-08-14 16:24:50 +0200

    Intialize Version with a default value.

--- Common/News/HomagConnect.News.Contracts/NewsArticle.cs
+++ Common/News/HomagConnect.News.Contracts/NewsArticle.cs
@@ -53,5 +53,5 @@ public sealed class NewsArticle
     /// <summary>
     /// Gets the version of the article.
     /// </summary>
-    public int? Version { get; init; }
+    public int Version { get; set; } = 1;
 }
