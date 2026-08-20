26808a86982e425f5ad75ceb250abd9c2d333c4f
joBueche <139100771+joBueche@users.noreply.github.com>
2026-08-20 15:55:01 +0200

    add missing translations for gateway (#843)
    Co-authored-by: Boris Wehrle (HOMAG) <141398900+HOMAG-Boris-Wehrle@users.noreply.github.com>

--- Applications/IntelliDivide/Contracts/Statistics/PartSizesByMaterialStatistic.cs
+++ Applications/IntelliDivide/Contracts/Statistics/PartSizesByMaterialStatistic.cs
@@ -1,33 +1,43 @@
-﻿using Newtonsoft.Json;
+﻿using System.ComponentModel.DataAnnotations;
+
+using HomagConnect.Base.Contracts.Attributes;
+using HomagConnect.Base.Contracts.Interfaces;
+
+using Newtonsoft.Json;
 
 namespace HomagConnect.IntelliDivide.Contracts.Statistics;
 
 /// <summary>
 /// Provides the part sizes by material statistic.
 /// </summary>
-public class PartSizesByMaterialStatistic
+[LocalizationResource(typeof(StatisticsDisplayNames))]
+public class PartSizesByMaterialStatistic : ISupportsLocalizedSerialization
 {
     /// <summary>
     /// Gets the length of the part.
     /// </summary>
     [JsonProperty(Order = 3)]
+    [Display(ResourceType = typeof(StatisticsDisplayNames), Name = nameof(Length))]
     public double Length { get; set; }
 
     /// <summary>
     /// Gets the material code of the part.
     /// </summary>
     [JsonProperty(Order = 1)]
+    [Display(ResourceType = typeof(StatisticsDisplayNames), Name = nameof(MaterialCode))]
     public string MaterialCode { get; set; }
 
     /// <summary>
     /// Gets the quantity of parts having the same <see cref="Length" />, <see cref="Width" /> and <see cref="MaterialCode" />.
     /// </summary>
     [JsonProperty(Order = 2)]
+    [Display(ResourceType = typeof(StatisticsDisplayNames), Name = nameof(Quantity))]
     public int Quantity { get; set; }
 
     /// <summary>
     /// Gets the width of the part.
     /// </summary>
     [JsonProperty(Order = 4)]
+    [Display(ResourceType = typeof(StatisticsDisplayNames), Name = nameof(Width))]
     public double Width { get; set; }
 }
