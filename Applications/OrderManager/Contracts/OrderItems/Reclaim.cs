1a0788fdc0fed9ba68e2c4b9e043c0af81244366
Manuel Schweikert <Manuel.Schweikert@homag.com>
2026-09-16 16:36:43 +0200

    Add reclaim item and props

---
+++ Applications/OrderManager/Contracts/OrderItems/Reclaim.cs
@@ -0,0 +1,33 @@
+﻿
+// Note: This is preliminary code and is subject to change
+
+namespace HomagConnect.OrderManager.Contracts.OrderItems;
+
+/// <summary>
+/// Represents a reclaim item in the order.
+/// </summary>
+public class Reclaim : Base
+{
+    /// <summary>
+    /// Gets or sets the reason for reclaiming the item.
+    /// </summary>
+    public ReclaimReason Reason { get; set; }
+
+    /// <summary>
+    /// Gets or sets the comment for the reclaim item.
+    /// </summary>
+    public string? Comment { get; set; }
+
+    /// <inheritdoc />
+    public override Type Type
+    {
+        get
+        {
+            return Type.Reclaim;
+        }
+        set
+        {
+            // Ignore
+        }
+    }
+}
